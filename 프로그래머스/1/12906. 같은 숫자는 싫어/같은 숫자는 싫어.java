183ef60e69bfd040a0f595104a97873ec631ad39
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-01-24 18:30:31 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 33.93 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -3,16 +3,16 @@ import java.util.*;
 public class Solution {
     public int[] solution(int []arr) {
         
-        Stack<Integer> stack = new Stack<>();
+        //Stack<Integer> stack = new Stack<>();
         ArrayList<Integer> alist = new ArrayList<>();
+        int preNum = arr[0];
         
-        stack.push(arr[0]);
         alist.add(arr[0]);
         for (int i = 1; i < arr.length; i++) {
-            if (arr[i] != stack.peek()) {
+            if (arr[i] != preNum) {
                 alist.add(arr[i]);
             }
-            stack.push(arr[i]);
+            preNum = arr[i];
         }
 
         return alist.stream().mapToInt(i -> i).toArray();
