10f7b107962cc88f664deae35a068db626100454
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-01-30 18:24:56 +0900

    [level 2] Title: 프로세스, Time: 1.73 ms, Memory: 91.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
 
 ### 성능 요약
 
-메모리: 79.7 MB, 시간: 1.71 ms
+메모리: 91.4 MB, 시간: 1.73 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 30일 17:08:01
+2025년 01월 30일 18:24:55
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,20 +2,20 @@ import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
         
-        Deque<int[]> deq = new ArrayDeque<>();
+        Queue<int[]> q = new LinkedList<>();
         Queue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
         int cnt = 1;
         
         for (int i = 0; i < priorities.length; i++) {
-            deq.offer(new int[] {i, priorities[i]}); //작업번호와 우선순위
+            q.offer(new int[] {i, priorities[i]}); //작업번호와 우선순위
             pq.offer(priorities[i]);
         }
         
         while (true && !pq.isEmpty()) {
-            int[] current = deq.poll();
+            int[] current = q.poll();
             //만일 현재 체크중인 deque의 작업이 우선순위가 밀린다면
             if (current[1]  < pq.peek()) {
-                deq.offer(current);//덱에 재삽입
+                q.offer(current);//덱에 재삽입
             }
             else {//만약 현재 체크중인게 같거나 크다면(불가능)
                 pq.poll();//제거
