d612efa7a3d25edcc62dba5dbd85fde840f75b02
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-03-05 00:08:31 +0900

    [level 2] Title: 큰 수 만들기, Time: 77.01 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,29 +1,24 @@
 import java.util.*;
 class Solution {
     public String solution(String number, int k) {
-        StringBuilder sb = new StringBuilder(number);
-        int i = 0;
-        // k가 남아있는 동안 반복
-        while (k > 0) {
-            // 현재 위치 i가 마지막 전까지 있을 경우
-            if (i < sb.length() - 1 && sb.charAt(i) < sb.charAt(i+1)) {
-                // i번째 숫자가 i+1번째보다 작으면 i번째 숫자 삭제
-                sb.deleteCharAt(i);
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+        int cut = number.length() - k;
+        
+        for (int i = 0; i < number.length(); i++) {
+            char num = number.charAt(i);
+            //이전꺼랑 비교하는 로직을 스택으로. 앞에꺼 대체가능한지 확인
+            while (!stack.isEmpty() && k > 0 && num > stack.peek()) {
+                stack.pop();
                 k--;
-                // 삭제 후 바로 앞 자리부터 재검사 (단, i가 0이면 그대로)
-                if (i > 0) {
-                    i--;
-                }
-            } else {
-                i++;
-                // 만약 i가 마지막 인덱스에 도달하면
-                if (i >= sb.length() - 1) {
-                    // 남은 k만큼 마지막 자리에서 삭제
-                    sb.delete(sb.length() - k, sb.length());
-                    k = 0;
-                }
             }
+            stack.push(num);
         }
-        return sb.toString();
+        
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        
+        return sb.reverse().substring(0, cut);
     }
 }
