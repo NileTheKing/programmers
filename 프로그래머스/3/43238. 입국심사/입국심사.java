ec93c3c4a1cd8bd9cd2795bb2b51670f850c4521
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-03-23 20:02:50 +0900

    [level 3] Title: 입국심사, Time: 95.84 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -4,36 +4,40 @@ class Solution {
         
         long answer = 0;
         Arrays.sort(times);
-        //long start = ((n / times.length) - 1) * times[0];
-        long start = 0;
-        long end = (long)n * times[times.length - 1]; //14
         
-        while (start <= end) {
-            long mid = start + (end - start) / 2; //30
+        long low = 1;
+        long high = (long)times[times.length - 1] * n;
+        
+        while (low <= high) {
+            long mid = low + (high - low) / 2; //중간 시간
             long donePeople = 0;
             
             for (int time : times) {
-                donePeople += mid / time;
+                donePeople += mid/time;
                 if (donePeople >= n) break;
             }
             
-    
-            if (donePeople < n) {
-                start = mid + 1;
-            }
-            else if (donePeople >= n) {
-                end = mid - 1;
+            if (donePeople >= n) {
+                high = mid - 1;
                 answer = mid;
             }
+            else {
+                low = mid + 1;
+            }
+            
+            
         }
-        
         return answer;
     }
 }
 /**
-음 ... 일단 정렬을 하면 좋을 수도 있을듯?
-큐?는 딱히 필요없지 그냥 전체 시간 구하는 거니까.
-n이 0이 될떄 까지 하면 됨. 시작하자마 times.length만큼 줄이고
-7, 10이 지날때마다 -해서 0될때까지
+만약 10,10이라면
+0분 4명남고
+10분 2명나목
+20분 0명남음
 
+10 * (n / 창구수)
+
+low와 high(가장 오래걸리는 창구) 를 두고 평균값은 mid를 구해 mid시간으로 해결할 수 있는지 확인
+해결할 수 있는지 확인: mid동안 처리한 사람수를 카운트해서 gt n or lt n등 케이스별로 low/high 수정
 */
