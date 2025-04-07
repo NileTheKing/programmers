bb0501b6ec1b3afaa1a5681c38bb2bcda9038dbe
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-04-08 00:50:35 +0900

    [level 1] Title: 체육복, Time: 2.14 ms, Memory: 99.3 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#) 
 
 ### 성능 요약
 
-메모리: 74.1 MB, 시간: 2.99 ms
+메모리: 99.3 MB, 시간: 2.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 22일 18:42:40
+2025년 04월 08일 00:50:31
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,36 +1,57 @@
 import java.util.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int cnt = 0;
-        HashSet<Integer> reserveSet = new HashSet<>();
+        Arrays.sort(lost);
+            
+        //있는지 없는지 비교해야하므로 자료구조 필요. 리스트는 삭제가 오래걸리고 그러므로 해시사용
+        Set<Integer> reserveSet = new HashSet<>();
+        Set<Integer> lostSet = new HashSet<>();
         for (int r : reserve) {
             reserveSet.add(r);
         }
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
-        
-        //잃어버리고 여분있던 애 처리
-        List<Integer> realNo = new ArrayList<>();
         for (int l : lost) {
-            if (reserveSet.contains(l)) {
-                cnt++;
-                reserveSet.remove(l);
-            } else {
-                realNo.add(l);
-            }
+            lostSet.add(l);
         }
         
-        //명단에 없는 애 처리
-        for (int l : realNo) {
-            if (reserveSet.contains(l - 1)) {
-                cnt++;
-                reserveSet.remove(l - 1);
+        Set<Integer> intersections = new HashSet<>(reserveSet);
+        intersections.retainAll(lostSet);
+        for (int i : intersections) {
+            reserveSet.remove(i);
+            lostSet.remove(i);
+        }
+        
+        //진짜 없던 애들을 가려야함
+        for (int loser : new TreeSet<>(lostSet)) {
+            int front = loser - 1;
+            int behind = loser + 1;
+            
+            if (reserveSet.contains(front)) {
+                reserveSet.remove(front);
+                lostSet.remove(loser);
             }
-            else if (reserveSet.contains(l + 1)) {
-                cnt++;
-                reserveSet.remove(l + 1);
+            else if (reserveSet.contains(behind)) {
+                reserveSet.remove(behind);
+                lostSet.remove(loser);
             }
         }
-        return n - lost.length + cnt;
+        return n - lostSet.size();
     }
 }
+
+/**
+어떻게 빌려주는 것이 가장 이득이냐?
+    완탐
+    그리디: 빌려줄 수 있는데 안빌려주는 경우가 더 나은 해가 있는 경우에는 그리디 사용불가. 그것이 아니면 이용 무조건 가능이지.
+
+    1 2 3 4 5학생이 있고 2,4는 없고 1,3,5가 예비가 있다면
+    1은 2한테 빌려주고 3은 4에게 빌려준다면 모두가 수업들을 수 있음
+    
+    1 2 3 4 5 학생이있고 2,4가 없고 3이 예비
+    3은 일단 앞에놈 빌려주면 ㅇㅋ
+    
+    그러면 앞에서 안빌려주고 뒤에다가 아껴서 빌려주면 이득인 경우가 있냐 이건데
+    어차피 앞뒤로밖에 못빌려주는 조건이 있어서 빌려줄 수 있으면 빌려줘야함. 만약 아꼈는데
+    다음놈이 필요없으면 기회를 날리는것이잖아
+    수학적..
+
+*/
