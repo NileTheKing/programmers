0033961c208ddcb482d69c82d94649a4252aa75a
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-04-07 19:24:35 +0900

    [level 1] Title: 모의고사, Time: 3.73 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,38 +1,28 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
+        int[] supo1 = {1,2,3,4,5};
+        int[] supo2 = {2,1,2,3,2,4,2,5};
+        int[] supo3 = {3,3,1,1,2,2,4,4,5,5};
         
-        int[] supo1 = {1, 2, 3, 4, 5};
-        int[] supo2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] supo3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        
-        int cnt1 = 0, cnt2 = 0, cnt3 = 0;
+        int[] cnts = new int[3];
         
         for (int i = 0; i < answers.length; i++) {
-            if (supo1[i % 5] == answers[i]) cnt1++;
-            if (supo2[i % 8] == answers[i]) cnt2++;
-            if (supo3[i % 10] == answers[i]) cnt3++;
+            if (supo1[i % supo1.length] == answers[i]) cnts[0]++;
+            if (supo2[i % supo2.length] == answers[i]) cnts[1]++;
+            if (supo3[i % supo3.length] == answers[i]) cnts[2]++;
         }
         
+        int max  = Integer.MIN_VALUE;
+        for (int cnt : cnts) {
+            max = Math.max(cnt, max);
+        }
         
-        //답구하기
-        int max = Math.max(cnt1, Math.max(cnt2, cnt3));
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < cnts.length; i++) {
+            if (cnts[i] >= max) answer.add(i+1);
+        }
         
-        HashSet<Integer> ans = new HashSet<>();
-        if (cnt1 == max) {
-            ans.add(1);
-        }
-        if (cnt2 == max) {
-            ans.add(2);
-        }
-        if (cnt3 == max) {
-            ans.add(3);
-        }
-        return ans.stream().mapToInt(i -> i).toArray();
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 }
-/**
-1번 1 2 3 4 5
-2번 2 하나 1 2 3 4 5
-3번 33 11 22 44 55
-*/
