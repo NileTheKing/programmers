3eb636c8a6e06f6cf3bc06a48abb3a700d11b723
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-04-07 18:47:32 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 2.87 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,52 +1,53 @@
 import java.util.*;
 class Solution {
-    private int answer = Integer.MAX_VALUE;
     public int solution(int n, int[][] wires) {
-        List<Integer>[] graph = new ArrayList[n + 1];
         
-        // 그래프 초기화
+        List<Integer>[] graph = new ArrayList[n + 1];//todo 
+        int answer = Integer.MAX_VALUE;
+        
         for (int i = 1; i <= n; i++) {
             graph[i] = new ArrayList<>();
-        }
-        //그래프로 옮기기
-        for (int[] wire: wires) {
-            int node1 = wire[0];
-            int node2 = wire[1];
-            graph[node1].add(node2);
-            graph[node2].add(node1);
+        } //
+    
+        //그래프 완성
+        for (int[] wire : wires) {
+            int v1 = wire[0];
+            int v2 = wire[1];
+            
+            graph[v1].add(v2);
+            graph[v2].add(v1);
         }
         
+        //하나씩 잘라가며 최솟차이값을 저장.dfs활용
         for (int[] wire : wires) {
-            int tmp1 = wire[0];
-            int tmp2 = wire[1];
-            graph[tmp1].remove(Integer.valueOf(tmp2));
-            graph[tmp2].remove(Integer.valueOf(tmp1));
-            int part1 = dfs(graph, 1, new boolean[n+1]);
+            int from = wire[0];
+            int to = wire[1];
+            
+            //삭제
+            graph[from].remove(Integer.valueOf(to));
+            graph[to].remove(Integer.valueOf(from));
+            //dfs
+            int part1 =  dfsAndCount(1, new boolean[n + 1], graph);
             int part2 = n - part1;
             answer = Math.min(answer, Math.abs(part1 - part2));
-            graph[tmp1].add(tmp2);
-            graph[tmp2].add(tmp1);
-            
+            //복구
+            graph[from].add(to);
+            graph[to].add(from);
         }
-        
         return answer;
-        
-    
     }
-    public int dfs(List<Integer>[] graph, int current, boolean[] visited) {
+    
+    int dfsAndCount(int current, boolean[] visited, List<Integer>[] graph) {
         int count = 1;
         visited[current] = true;
-        for (int neighbor : graph[current]) {
-            if (!visited[neighbor]) {
-                visited[neighbor] = true;
-                count += dfs(graph, neighbor, visited);
+        
+        for (int next : graph[current]) {
+            if (!visited[next]) {
+                visited[next] = true;
+                count += dfsAndCount(next, visited, graph);
             }
         }
-        return count;
         
+        return count; 
     }
 }
-/**
-백트래킹으로 한번에 한다면?
-그냉 메인함수에서 다리 연결끊고 dfs돌려서 차이 구하면서 최솟값 갱신하는 것이 더 낫지 않을까 함
-*/
