004d47f0b47972c83db9a30db4917747f13b9568
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-04-15 20:10:15 +0900

    [level 2] Title: 큰 수 만들기, Time: 92.09 ms, Memory: 98 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,24 +1,49 @@
 import java.util.*;
 class Solution {
     public String solution(String number, int k) {
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder(number);
         Stack<Character> stack = new Stack<>();
-        int cut = number.length() - k;
+        stack.push(number.charAt(0));
+        int ogk = k; //k값 복사
         
-        for (int i = 0; i < number.length(); i++) {
-            char num = number.charAt(i);
-            //이전꺼랑 비교하는 로직을 스택으로. 앞에꺼 대체가능한지 확인
-            while (!stack.isEmpty() && k > 0 && num > stack.peek()) {
+        for (int i = 1; i < number.length(); i++) {
+            char c = number.charAt(i);
+            //앞에 더 작은 게 없으면 다 없앰
+            while (!stack.isEmpty() && c > stack.peek() && k > 0) {
                 stack.pop();
                 k--;
             }
-            stack.push(num);
+            stack.push(c);
         }
         
         while (!stack.isEmpty()) {
             sb.append(stack.pop());
         }
         
-        return sb.reverse().substring(0, cut);
+        
+        return sb.reverse().substring(0, number.length() - ogk);
+        
     }
 }
+/**
+숫자 제거
+작은거를 제거하면 좋고
+큰거는 남겨놓으면 좋지
+    뒤에 자릿수가 더 크면 지우면 됨
+    계쏙 인접한거 비교할 수 있게 하면 좋을듯?
+    
+
+1924면 19에서 1지워야지. 최소 1보단 나으니까
+그러면 924
+그러고나서 924 92. 그다음 24. 2지우는게 이득 94
+
+그렇다면 매번 지울떄마다 앞에서부터 비교. 
+while (k) {
+    for ()
+}
+
+1231234
+    231234
+    31234
+    3234
+*/
