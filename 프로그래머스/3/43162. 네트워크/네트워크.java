4fb19d2989c9001a07b1735d6ab6bda6b7183af1
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-04-18 10:29:11 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 92.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,40 +1,26 @@
 class Solution {
     public int solution(int n, int[][] computers) {
-        int cnt = 0;
-        
-        boolean[] visited = new boolean[computers.length];
-        
-        for (int i = 0; i < computers.length; i++) {
-            if (visited[i]) {
-                continue;
-            }
-            else {
-                dfs(computers, visited, i);
-                cnt++;
+        int answer = 0;
+        boolean[] visited = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                answer++;
+                dfs(computers, i, visited);
             }
         }
-        return cnt;
+        return answer;
     }
     
-    public void dfs(int[][] computers, boolean[] visited, int current) {
+    void dfs(int[][] computers, int current, boolean[] visited) {
         
-        if (visited[current]) return;
         visited[current] = true;
-        
-        //현재 확인중인 컴퓨터의 연결상태 확인
-        for (int i = 0; i < visited.length; i++) {
-            if (current!= i && computers[current][i] == 1) {
-                dfs(computers, visited, i);
+        for (int i = 0; i < computers.length; i++) {
+            if (computers[current][i] == 1 && !visited[i] && current != i) {
+                dfs(computers, i, visited);
             }
         }
-        
-        return;
     }
 }
 /**
-몇덩어리로 나누어 지냐
-그래프
-이거 리트코드에서 풀어본듯? 그래프 그냥 주어진 배열 그대로 써도 될듯? visited정도만 해가지고 하면 될듯.
 
-visited처리를 들어가기 전에 하나?
 */
