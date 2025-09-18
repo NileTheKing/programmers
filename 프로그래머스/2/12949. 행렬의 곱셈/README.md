794ad33ba973161c1d61e3652f938927a9f1a6aa
양나일, Nile Yang <129058095+NileTheKing@users.noreply.github.com>
2025-09-18 09:36:28 +0900

    [level 2] Title: 행렬의 곱셈, Time: 63.39 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/README.md
+++ 프로그래머스/2/12949. 행렬의 곱셈/README.md
@@ -1,10 +1,10 @@
 # [level 2] 행렬의 곱셈 - 12949 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12949) 
 
 ### 성능 요약
 
-메모리: 10 MB, 시간: 122.52 ms
+메모리: 10 MB, 시간: 63.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 11일 13:43:36
+2025년 09월 18일 09:36:26
 
 ### 문제 설명
 
--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,12 +1,12 @@
 def solution(arr1, arr2):
-    m = len(arr1)
-    n = len(arr1[0])
-    l = len(arr2[0])
-    ans = [[0 for _ in range(l)] for _ in range(m)]
-    for i in range(m) :
+    m, n, l = len(arr1), len(arr1[0]), len(arr2[0])
+    # m * l이 결과로 나올것
+    ans = [[0] * l for _ in range(m)]
+    for i in range(m):
         for j in range(l):
+            # (i,j)
             sum = 0
-            for k in range(n) :
-                ans[i][j] += arr1[i][k] * arr2[k][j]
-    
+            for k in range (n):
+                sum += arr1[i][k] * arr2[k][j]
+            ans[i][j] = sum
     return ans
