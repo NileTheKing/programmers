3b5f3cc21937127269b4a73b8c2c7d1b0efd2b81
양나일, Nile Yang <nileyangdev@gmail.com>
2025-09-29 16:55:58 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 1.21 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -32,7 +32,7 @@ class Solution {
         if (pos_sec >= op_start_sec && pos_sec <= op_end_sec) { //현재위치가 스킵시간이라면 스킵
                 //System.out.printf("skipping at: %d \n", pos_sec);
                 pos_sec = op_end_sec;
-            }
+        }
         //현재 시간을 "mm:ss" 형식으로 변환
         //13:00이라면 현재 13*60인 780일거임.
         //780 / 60인 13을 mm에넣고
@@ -41,29 +41,21 @@ class Solution {
         int minute = pos_sec / 60;
         int sec = pos_sec % 60;
         //정수를 string으로..... 배열에 담아서 읽기? 흠
-        if(minute < 10) {
-            ans.append("0");
-            ans.append(String.valueOf(minute));
-        }else {
-            ans.append(String.valueOf(minute));
-        }
-        ans.append(":");
-        if(sec < 10) {
-            ans.append("0");
-            ans.append(String.valueOf(sec));
-        }else {
-            ans.append(String.valueOf(sec));
-        }
-        return ans.toString();
+        return String.format("%02d:%02d", minute, sec);
 
 
     }
     public int toSeconds(String time) { //분을 해당 시간으로 변환
-        int videoMinuteLength;
-        int videoSecondLength;
-        videoMinuteLength = Integer.parseInt(time.substring(0,2)); //비디오 길이 분
-        videoSecondLength = Integer.parseInt(time.substring(3,time.length())); //비디오 길이 초
-        return videoMinuteLength * 60 + videoSecondLength;
+        // int videoMinuteLength;
+        // int videoSecondLength;
+        // videoMinuteLength = Integer.parseInt(time.substring(0,2)); //비디오 길이 분
+        // videoSecondLength = Integer.parseInt(time.substring(3,time.length())); //비디오 길이 초
+        // return videoMinuteLength * 60 + videoSecondLength;
+        String[] parts = time.split(":");
+        int min = Integer.parseInt(parts[0]);
+        int sec = Integer.parseInt(parts[1]);
+        
+        return min * 60 + sec;
     }
 }
 /**
