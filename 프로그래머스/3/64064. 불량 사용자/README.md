bd2ac88091f2540a1b4e28385693fd71674e902f
양나일, Nile Yang <nileyangdev@gmail.com>
2025-09-29 10:41:08 +0900

    [level 3] Title: 불량 사용자, Time: 23.28 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/README.md
+++ 프로그래머스/3/64064. 불량 사용자/README.md
@@ -1,10 +1,10 @@
 # [level 3] 불량 사용자 - 64064 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64064) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64064?language=java) 
 
 ### 성능 요약
 
-메모리: 9.17 MB, 시간: 90.90 ms
+메모리: 78.7 MB, 시간: 23.28 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 17일 11:22:03
+2025년 09월 29일 10:41:05
 
 ### 문제 설명
 
--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -1,54 +1,44 @@
 import java.util.*;
-
 class Solution {
-    Set<Integer> finalCombinations = new HashSet<>();
+    List<List<Integer>> candidates;
+    Set<Integer> set = new HashSet<>();
     Map<String, Integer> userIdx = new HashMap<>();
-    List<List<Integer>> ways; // 인덱스별 후보 저장
-
     public int solution(String[] user_id, String[] banned_id) {
-        // 유저 인덱스 매핑
+        candidates = new ArrayList<>();
         for (int i = 0; i < user_id.length; i++) {
             userIdx.put(user_id[i], i);
         }
-
-        // banned_id 개수만큼 리스트 준비
-        ways = new ArrayList<>();
-        for (int i = 0; i < banned_id.length; i++) {
-            ways.add(new ArrayList<>());
-        }
-
-        // 각 banned_id별 후보 찾기
-        for (int i = 0; i < banned_id.length; i++) {
-            String ban = banned_id[i];
+        for (String ban : banned_id) {
+            // fr*d*에 해당할 수 있는 user_id들 모아서 추가해줌
+            List<Integer> users = new ArrayList<>();
+            String regex_ban = ban.replaceAll("\\*", ".");
             for (String user : user_id) {
-                if (match(ban, user)) {
-                    ways.get(i).add(userIdx.get(user));
+                if (user.matches(regex_ban)) {
+                    users.add(userIdx.get(user));
                 }
             }
+            candidates.add(users);
         }
-
-        backtrack(banned_id.length, 0, 0);
-        return finalCombinations.size();
+        
+        //이제 banned_id는 가능성있는 유저 리스트들이 있음.
+        //backtracking해서 카운팅해야함.
+        backtrack(user_id, banned_id, 0, 0);
+        
+        return set.size();
+        
     }
-
-    public void backtrack(int banCount, int idx, int mask) {
-        if (idx == banCount) {
-            finalCombinations.add(mask);
+    public void backtrack(String[] user_id, String[] banned_id, int idx, int current) {
+        if (idx == banned_id.length) {
+            //기존에 있는 조합인지 비교해서 추가. 이떄 비트마스킹을 사용해서 빠르게 비ㅐ교
+            if (!set.contains(current)) {
+                set.add(current);
+            }
             return;
         }
-
-        for (int user : ways.get(idx)) {
-            if ((mask & (1 << user)) != 0) continue;
-            backtrack(banCount, idx + 1, mask | (1 << user));
+        
+        for (int user : candidates.get(idx)) {
+            if ((current & (1 << user)) != 0) continue;
+            backtrack(user_id, banned_id, idx+1, current | 1 << user);
         }
-    }
-
-    private boolean match(String ban, String user) {
-        if (ban.length() != user.length()) return false;
-        for (int j = 0; j < user.length(); j++) {
-            if (ban.charAt(j) == '*') continue;
-            if (ban.charAt(j) != user.charAt(j)) return false;
-        }
-        return true;
     }
 }
