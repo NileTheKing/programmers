391a37f5a3339e872c8868dd220ccb442ee209ce
양나일, Nile Yang <nileyangdev@gmail.com>
2025-09-29 13:11:16 +0900

    [level 2] Title: 괄호 회전하기, Time: 65.04 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/README.md
+++ 프로그래머스/2/76502. 괄호 회전하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 회전하기 - 76502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502?language=java) 
 
 ### 성능 요약
 
-메모리: 9.3 MB, 시간: 150.42 ms
+메모리: 84.5 MB, 시간: 65.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 17일 13:37:36
+2025년 09월 29일 13:11:13
 
 ### 문제 설명
 
--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,45 +1,43 @@
 import java.util.*;
-
 class Solution {
     public int solution(String s) {
-        int count = 0;
-        int n = s.length();
-
-        for (int i = 0; i < n; i++) {
-            Stack<Character> stack = new Stack<>();
-            boolean isValid = true;
-
-            for (int j = 0; j < n; j++) {
-                char c = s.charAt((i + j) % n);
-
-                if (c == '(' || c == '[' || c == '{') {
-                    stack.push(c);
-                } else {
-                    // 1. (버그 수정) 스택이 비어있는데 닫는 괄호가 나오면 실패
-                    if (stack.isEmpty()) {
-                        isValid = false;
-                        break;
+        Map<Character, Character> map = new HashMap<>();
+        Set<Character> open = new HashSet<>();
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+        for (char c : map.values()) {
+            //System.out.printf("%c added\n", c);
+            open.add(c);
+        }
+        int cnt = 0;
+        for (int i = 0; i < s.length(); i++) { //각 시작점
+            //끝까지 순회
+            //새로운스택
+            Stack<Character> stk = new Stack<>();
+            boolean available = true;
+            for (int j = 0; j < s.length(); j++) {
+                int current = (i + j) % s.length();// 012345이 있고 2부터시작이면 234501.
+                char c = s.charAt(current);
+                if (open.contains(c)) {//지ㅐ금 넣는게 여는괄호
+                    stk.push(c);
+                }else { //닫는괄호
+                    if (stk.isEmpty()) {
+                        available = false;
+                        break; //불가능
                     }
-
-                    char top = stack.peek();
-                    
-                    // 2. (간결화) 짝이 맞는 경우를 하나로 묶어 처리
-                    if ((c == ')' && top == '(') || 
-                        (c == ']' && top == '[') || 
-                        (c == '}' && top == '{')) {
-                        stack.pop();
-                    } else {
-                        // 짝이 맞지 않으면 실패
-                        isValid = false;
+                    char top = stk.peek();
+                    if (top == map.get(c)) { //짝맞음
+                        stk.pop();
+                    }else {//짝안맞음
+                        available = false;
                         break;
                     }
                 }
             }
-
-            if (isValid && stack.isEmpty()) {
-                count++;
-            }
+            //플래그가 가능하면 카운트늘리기
+            if (available && stk.isEmpty()) cnt++;
         }
-        return count;
+        return cnt;
     }
 }
