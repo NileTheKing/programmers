f0d1fee05d01e0cf11a469971f59f75f6e3850bd
양나일, Nile Yang <nileyangdev@gmail.com>
2025-10-12 18:29:13 +0900

    [level 3] Title: 불량 사용자, Time: 35.98 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/README.md
+++ 프로그래머스/3/64064. 불량 사용자/README.md
@@ -1,10 +1,10 @@
 # [level 3] 불량 사용자 - 64064 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64064?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64064) 
 
 ### 성능 요약
 
-메모리: 78.7 MB, 시간: 23.28 ms
+메모리: 82.1 MB, 시간: 35.98 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 29일 10:41:05
+2025년 10월 12일 18:29:08
 
 ### 문제 설명
 
--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.java
@@ -1,44 +1,34 @@
 import java.util.*;
 class Solution {
-    List<List<Integer>> candidates;
-    Set<Integer> set = new HashSet<>();
-    Map<String, Integer> userIdx = new HashMap<>();
+    Map<String, Integer> userId2number = new HashMap<>();
+    List<List<String>> candidates = new ArrayList<>();
+    Set<Integer> combinations = new HashSet<>();
     public int solution(String[] user_id, String[] banned_id) {
-        candidates = new ArrayList<>();
         for (int i = 0; i < user_id.length; i++) {
-            userIdx.put(user_id[i], i);
+            userId2number.put(user_id[i], i);
         }
         for (String ban : banned_id) {
-            // fr*d*에 해당할 수 있는 user_id들 모아서 추가해줌
-            List<Integer> users = new ArrayList<>();
-            String regex_ban = ban.replaceAll("\\*", ".");
+            String regex = ban.replaceAll("\\*", ".");
+            List<String> match = new ArrayList<>();
             for (String user : user_id) {
-                if (user.matches(regex_ban)) {
-                    users.add(userIdx.get(user));
-                }
+                if (user.matches(regex)) match.add(user);
             }
-            candidates.add(users);
+            candidates.add(match);
         }
         
-        //이제 banned_id는 가능성있는 유저 리스트들이 있음.
-        //backtracking해서 카운팅해야함.
-        backtrack(user_id, banned_id, 0, 0);
-        
-        return set.size();
-        
+        dfs(banned_id, 0, 0);
+        return combinations.size();
     }
-    public void backtrack(String[] user_id, String[] banned_id, int idx, int current) {
-        if (idx == banned_id.length) {
-            //기존에 있는 조합인지 비교해서 추가. 이떄 비트마스킹을 사용해서 빠르게 비ㅐ교
-            if (!set.contains(current)) {
-                set.add(current);
-            }
+    public void dfs(String[] banned_id, int banIdx, int current) {
+        if (banIdx == banned_id.length) {
+            combinations.add(current);
             return;
         }
         
-        for (int user : candidates.get(idx)) {
-            if ((current & (1 << user)) != 0) continue;
-            backtrack(user_id, banned_id, idx+1, current | 1 << user);
+        for (String can : candidates.get(banIdx)) {
+            int userIdx = userId2number.get(can);
+            if ((1 << userIdx & current) != 0) continue;
+            dfs(banned_id, banIdx + 1, ((1 << userIdx) | current));
         }
     }
 }
