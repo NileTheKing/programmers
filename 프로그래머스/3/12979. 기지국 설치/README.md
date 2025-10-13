577db88517422511214f7ab58a1f5d0107d8a29d
양나일, Nile Yang <nileyangdev@gmail.com>
2025-10-13 23:06:45 +0900

    [level 3] Title: 기지국 설치, Time: 2.52 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/README.md
+++ 프로그래머스/3/12979. 기지국 설치/README.md
@@ -1,10 +1,10 @@
 # [level 3] 기지국 설치 - 12979 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12979?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12979#qna) 
 
 ### 성능 요약
 
-메모리: 54.5 MB, 시간: 2.75 ms
+메모리: 54.7 MB, 시간: 2.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 29일 11:12:50
+2025년 10월 13일 23:06:40
 
 ### 문제 설명
 
--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,22 +1,21 @@
 import java.util.*;
 class Solution {
     public int solution(int n, int[] stations, int w) {
-        int coverageWidth = 2 * w + 1;
+        int coverageWidth = w * 2 + 1;
         int lastCovered = 0;
-        int cnt = 0;
+        int ans = 0;
         for (int s : stations) {
             int startCovering = s - w;
-            int gap = startCovering - lastCovered - 1; // 3 - 0 - 1 = 2
-            int cntThisTime = (int)Math.ceil((double)gap / coverageWidth);
-            //System.out.printf("from %d to %d, gap  %d, cnt this time: %d\n", lastCovered, startCovering, gap, cntThisTime);
-            cnt += cntThisTime;
+            int gap = startCovering - lastCovered - 1;
+            int cnt = (int)Math.ceil((double)gap / coverageWidth);
+            ans += cnt;
+            //System.out.printf("startCovering %d, gap %d, coveraageWidth %d, cnt %d\n", startCovering, gap, coverageWidth, cnt);
             lastCovered = s + w;
         }
-        
         if (lastCovered < n) {
             int gap = n - lastCovered;
-            cnt += (int)Math.ceil((double)gap / coverageWidth);
+            ans += (int)Math.ceil((double)gap / coverageWidth);
         }
-        return cnt;
+        return ans;
     }
 }
