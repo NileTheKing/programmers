2aab8e7eded1f46c6efd65e96a1d85429ed7c643
양나일, Nile Yang <nileyangdev@gmail.com>
2026-02-04 23:42:59 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+select member_id, member_name, gender, DATE_FORMAT(date_of_birth, '%Y-%m-%d') as date_of_birth
+from member_profile
+where MONTH(date_of_birth) = 3 
+    and gender = 'W' 
+    and tlno is not null
+order by member_id asc;
