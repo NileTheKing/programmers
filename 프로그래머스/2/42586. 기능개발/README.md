085189c6831a86db83df84fd6a60d9574ce8d72e
양나일, Nile Yang <nileyangdev@gmail.com>
2026-02-26 18:33:20 +0900

    [level 2] Title: 기능개발, Time: 3.14 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 84.1 MB, 시간: 2.65 ms
+메모리: 89.1 MB, 시간: 3.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 28일 19:01:51
+2026년 02월 26일 18:33:18
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,31 +1,28 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
+        List<Integer> ans = new ArrayList<>();
         
-        List<Integer> answer = new ArrayList<>();
-        int[] days = new int[progresses.length];
-        
-        for (int i = 0; i < days.length; i++) {
-            //days[i] = (100 - progresses[i] + speeds[i] - 1) / speeds[i];
-            days[i] = (100 - progresses[i] + speeds[i] - 1) / speeds[i];
+        int length = progresses.length;
+        int[] days = new int[length];
+        for (int i = 0; i < length; i++) {
+            days[i] = (int)Math.ceil((double)(100-progresses[i]) / speeds[i]);
         }
         
+        //days: {7,3,9}
         int max = days[0];
-        int count = 1;
+        int cnt = 1;
         for (int i = 1; i < days.length; i++) {
-            //현재 확인하는 작업의 남은 일수가 기존의 것보다 크다면
-            // 7 3 9에서 기존거는 
-            if (days[i] <= max) {
-                count++;
-            }
-            else {
-                max = days[i]; //최댓값 갱신
-                answer.add(count);//답추가
-                count = 1;//카운트 초기화
+            if (days[i] <= max) {//한번에 배포가능
+                cnt++;
+            }else {//이젠 안된다. 처리
+                ans.add(cnt);
+                cnt = 1;//초기화
+                max = days[i];
             }
         }
-        answer.add(count);
+        ans.add(cnt);
         
-        return answer.stream().mapToInt(i -> i).toArray();
+        return ans.stream().mapToInt(i->i).toArray();
     }
 }
