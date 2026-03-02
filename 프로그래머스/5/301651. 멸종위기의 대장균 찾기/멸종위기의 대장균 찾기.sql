ff1f9fb8ad5fd7363b0d4229d4ae248c9a5b2c01
양나일, Nile Yang <nileyangdev@gmail.com>
2026-03-02 15:26:00 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -0,0 +1,21 @@
+    -- 코드를 작성해주세요
+    with recursive generation as (
+        select id, parent_id, 1 as gen
+        from ecoli_data
+        where parent_id is null
+
+        union all
+
+        select e.id, e.parent_id, g.gen + 1
+        from ecoli_data e
+        join generation g on e.parent_id = g.id
+    )
+
+    select count(*) as count, g.gen as generation
+    from generation g
+    left join ecoli_data child on g.id = child.parent_id
+    where child.id is null
+    group by g.gen
+    order by gen;
+
+    #
