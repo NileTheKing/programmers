ce91c2701d325c3727db99a8bc070de4abbea01c
양나일, Nile Yang <nileyangdev@gmail.com>
2026-03-05 20:51:29 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,13 +1,7 @@
 -- 코드를 작성해주세요
-select id
-from ecoli_data
-where parent_id in ( #2세대인 애들의 id
-    select id
-    from ecoli_data
-    where parent_id in ( #1세대인 애들의 id
-        select id
-        from ecoli_data
-        where parent_id is null
-    )
-)
+select g3.id
+from ecoli_data g1
+join ecoli_data g2 on g1.id = g2.parent_id #이제 g1은 자기자식을 줄줄 달고있음
+join ecoli_data g3 on g2.id = g3.parent_id #이제 g2는 자기 자식을 줄줄 달고있음.
+where g1.parent_id is null
 order by id asc;
