5868d6e7fdb9be427ce5a96bb0b570b1a0939be2
양나일, Nile Yang <nileyangdev@gmail.com>
2026-03-05 15:44:09 +0900

    [level 2] Title: 가격대 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
+++ 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -1,5 +1,6 @@
 -- 코드를 입력하세요
-SELECT truncate(price, -4) as price_group, count(*) as products
+SELECT floor(price / 10000) * 10000 as price_group, count(*) as products
 from product
-group by truncate(price, -4)
-order by price_group asc;
+group by floor(price / 10000)
+order by price_group;
+#case when은 안됨...노가다니까 각바구니에 이름을 붙여줘야하는디..
