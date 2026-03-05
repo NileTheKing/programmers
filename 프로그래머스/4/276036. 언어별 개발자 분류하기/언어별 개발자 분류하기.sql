f0b15d69891985b9f17e834f5c4abff8dd9cb840
양나일, Nile Yang <nileyangdev@gmail.com>
2026-03-05 17:35:03 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,16 +1,13 @@
 -- 코드를 작성해주세요
 select 
     case
-        when skill_code & (select sum(code) from skillcodes where category = 'Front End') and skill_code & (select code from skillcodes where name = 'Python') then 'A'
+        when skill_code & (select sum(code) from skillcodes where category = 'front end') and skill_code & (select code from skillcodes where name = 'python') then 'A'
         when skill_code & (select code from skillcodes where name = 'C#') then 'B'
-        when skill_code & (select sum(code) from skillcodes where category = 'Front End') then 'C'
-    end as grade
-    , id, email
+        when skill_code & (select sum(code) from skillcodes where category = 'front end') then 'C'
+    end as grade,
+    ID, email
 from developers
-where (id) in (
-    select id
-    from developers
-    where skill_code & (select sum(code) from skillcodes where category = 'Front End') and skill_code & (select code from skillcodes where name = 'Python') or skill_code & (select code from skillcodes where name = 'C#') or skill_code & (select sum(code) from skillcodes where category = 'Front End')
-)
-group by grade, id
-order by grade, id;
+where skill_code & skill_code & (select sum(code) from skillcodes where category = 'front end') and skill_code & (select code from skillcodes where name = 'python') or 
+skill_code & (select code from skillcodes where name = 'C#') or 
+skill_code & (select sum(code) from skillcodes where category = 'front end')
+order by grade, id asc;
