ddeea2df3021e62c1abf5d2178db112ba4e094dc
양나일, Nile Yang <nileyangdev@gmail.com>
2026-03-05 17:42:22 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -2,11 +2,13 @@
 with recursive time_table as (
     select 0 as hour
     union all
-    select hour + 1 from time_table where hour < 23
+    select hour + 1
+    from time_table
+    where hour < 23
 )
-SELECT t.hour, count(animal_id)
+
+SELECT t.hour as hour, count(i.animal_id) as count
 from time_table t
-left join animal_outs a
-on t.hour = hour(a.datetime)
+left join animal_outs i on t.hour = hour(i.datetime)
 group by t.hour
-order by t.hour
+order by hour asc;
