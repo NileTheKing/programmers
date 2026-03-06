d58a90a942e1326fbaa24c84648963ce0d931ea5
양나일, Nile Yang <nileyangdev@gmail.com>
2026-03-06 12:37:28 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -1,12 +1,10 @@
 -- 코드를 작성해주세요
-select i.item_id, i.item_name, i.rarity
-from item_info parent
-join item_tree child on parent.item_id = child.parent_item_id #이제 자식들이 줄줄달림
-join item_info i on child.item_id = i.item_id #자식들의 정보를 가져옴. 왜? 거기붙어있는 id로는 정보못뽐음
-where parent.rarity = 'rare'
-order by item_id desc;
-#하려는게.. 자식이 업그레이드 된 아이템이잖아 그치
-#부모의 아이템이 rare이고 ..그렇다면 걔 부모의 정보 뽑는거임.
-#그렇다면 테이블조인 or 재귀인데..다음거만 뽑으니까 서브쿼리1번으로 가능하지 재귀도 당연히되는데 그건 비효울적.
-#아무튼 parent item id를 item_info에서 출력하는것
-#일단 생각나는 서브쿼리로 가보자 select로 가야겠지?
+select i2.item_id, i2.item_name, i2.rarity
+from item_info i
+join item_tree t on i.item_id = t.parent_item_id #이제 각 아이템은 다음 업그레이드 아이템이있음
+join item_info i2 on t.item_id = i2.item_id
+where i.rarity = 'rare'
+order by i2.item_id desc;
+
+#서브쿼리 조인으로 풀 수 있음.
+#조인으로..
