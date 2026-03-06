b063be53751cc549c2ea07daa1788c7f2df45acb
양나일, Nile Yang <nileyangdev@gmail.com>
2026-03-06 13:32:26 +0900

    [level 1] Title: 잡은 물고기의 평균 길이 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/293259. 잡은 물고기의 평균 길이 구하기/잡은 물고기의 평균 길이 구하기.sql
+++ 프로그래머스/1/293259. 잡은 물고기의 평균 길이 구하기/잡은 물고기의 평균 길이 구하기.sql
@@ -1,10 +1,3 @@
 -- 코드를 작성해주세요
-select round(avg(length),2) as average_length
-from (
-    select 
-        case 
-            when length is null then 10
-            else length
-        end as length
-    from fish_info
-) as t
+select round(avg(ifnull(length,10)),2) as average_length
+from fish_info
