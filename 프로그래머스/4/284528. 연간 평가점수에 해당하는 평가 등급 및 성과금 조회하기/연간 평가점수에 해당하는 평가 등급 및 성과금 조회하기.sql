f9fda6979645ab82674efc6c92ef8a4d0d2de1db
양나일, Nile Yang <nileyangdev@gmail.com>
2026-03-06 14:19:58 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,18 +1,23 @@
 -- 코드를 작성해주세요
-select e.emp_no, e.emp_name, 
-    case
-        when avg(g.SCORE) >= 96 then 'S'
-        when avg(g.SCORE) >= 90 then 'A'
-        when avg(g.SCORE) >= 80 then 'B'
-        else 'C'
-    end as grade,
-    case
-        when avg(g.SCORE) >= 96 then e.sal * (0.2)
-        when avg(g.SCORE) >= 90 then e.sal * (0.15)
-        when avg(g.SCORE) >= 80 then e.sal * (0.1)
+select e.emp_no, e.emp_name, t.grade,
+    case    
+        when t.grade = 'S' then e.sal * 0.2
+        when t.grade = 'A' then e.sal * 0.15
+        when t.grade = 'B' then e.sal * 0.1
+        when t.grade = 'C' then 0
         else 0
     end as bonus
 from HR_EMPLOYEES e
-join hr_grade g on e.emp_no = g.emp_no
-group by e.emp_no
-order by e.emp_no asc;
+join (
+    select e.emp_no, 
+        case 
+            when avg(g.score) >= 96 then 'S'
+            when avg(g.score) >= 90 then 'A'
+            when avg(g.score) >= 80 then 'B'
+            else 'C'
+        end as grade
+    from hr_employees e
+    join hr_grade g on e.emp_no = g.emp_no
+    group by emp_no
+) as t on e.emp_no = t.emp_no
+order by e.emp_no;
