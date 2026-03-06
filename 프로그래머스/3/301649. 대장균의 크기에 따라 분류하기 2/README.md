45f49bd0bbe34c086a804adcdcd09eac287d9a21
양나일, Nile Yang <nileyangdev@gmail.com>
2026-03-06 13:31:18 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/README.md
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/README.md
@@ -1,6 +1,6 @@
 # [level 3] 대장균의 크기에 따라 분류하기 2 - 301649 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/301649#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/301649) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 03일 15:06:14
+2026년 03월 06일 13:31:17
 
 ### 문제 설명
 
--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,13 +1,13 @@
 -- 코드를 작성해주세요
 select id,
     case
-        when per <= 0.25 then 'CRITICAL'
-        when per <= 0.50 then 'HIGH'
-        when per <= 0.75 then 'MEDIUM'
-        else 'LOW'
+        when per >= 0.75 then 'LOW'
+        when per >= 0.5 then 'MEDIUM'
+        when per >= 0.25 then 'HIGH'
+        else 'CRITICAL'
     end as colony_name
 from (
     select id, percent_rank() over (order by size_of_colony desc) as per
     from ecoli_data
-) as t
+) as T
 order by id;
