f0573fab0a123337fb57b849c294faeb9a1b1578
양나일, Nile Yang <nileyangdev@gmail.com>
2026-03-06 19:57:14 +0900

    [level 2] Title: 비밀 코드 해독, Time: 9954.22 ms, Memory: 204 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.java
@@ -1,73 +1,69 @@
 import java.util.*;
 class Solution {
-    List<List<Integer>> allCombs = new ArrayList<>();
+    List<List<Integer>> combs = new ArrayList<>();
     public int solution(int n, int[][] q, int[] ans) {
+        getCombs(1, n, new ArrayList<>());
+        //조합 디버깅
+        // for (List<Integer> c : combs) {
+        //     System.out.printf("output comb\n");
+        //     for (int i : c) {
+        //         System.out.printf("%d ", i);
+        //     }
+        // }
         
-        combination(n, 1, new ArrayList<>());
-        //System.out.printf("allcombs size : %d\n", allCombs.size());
-        int res = 0;
-        for (List<Integer> comb : allCombs) {//각 조합에대해 q되는지
-            //System.out.printf("debug 347910\n");
-            //for (int c  :comb) System.out.printf("%d ", c);
-            // System.out.println();
-            boolean flag = true;
+        int cnt = 0;
+        for (List<Integer> c : combs) {
+            boolean able = true;
             for (int i = 0; i < q.length; i++) {
-                int match = ans[i];//q[i]랑 comb비교헤ㅐㅆ을때 match만큼 동일해야함
-                int cnt = compare(comb, q[i]);
-                //System.out.printf("match %d, cnt %d\n", match, cnt);
-                if (match == cnt) continue;
-                else {
-                    // System.out.printf("comb: ");
-                    // for (int c : comb) System.out.printf("%d ", c);
-                    // System.out.printf("and \n q: ");
-                    // for (int c : q[i]) System.out.printf("%d ", c);
-                    // System.out.printf("is diffent match, cnt : %d %d\n", match, cnt);
-                    flag = false;
+                int match = findMatch(c, q[i]);
+                if (match != ans[i]) {
+                    able = false;    
                     break;
                 }
             }
-            //가능
-            // System.out.printf("possible ");
-            // for (int c : comb) System.out.printf("%d ",c);
-            // System.out.println();
-            if (flag) res++;
-        }
-        return res;
-    }
-    public void combination(int n, int idx, List<Integer> current) {
-        if (current.size() == 5) {
-            //System.out.printf("combination added debug\n");
-            //for (int c : current) System.out.printf("%d ", c);
-            //System.out.println();
-            allCombs.add(new ArrayList<>(current));
-        }
-        
-        for (int i = idx; i <= n; i++) { //1부터 30까지
-            current.add(i);
-            combination(n, i + 1, current);
-            current.remove(current.size() - 1);
-        }
-        return;
-    }
-    public int compare(List<Integer> list, int[] array) {
-        int length = 5;
-        int cnt = 0;
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < length; j++) {
-                if (list.get(i) == array[j]) {
-                    cnt++;
-                    break;
-                }   
-            }
+            //다 통과하면 카운트
+            if (able) cnt++;
         }
         return cnt;
     }
+    public int findMatch(List<Integer> o1, int[] o2) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        
+        for (int i : o1) {
+            set1.add(i);
+        }
+        for (int i  : o2) {
+            set2.add(i);
+        }
+        
+        int cnt = 0;
+        for (int num1 : set1) {
+            if (set2.contains(num1)) cnt++;
+        }
+        return cnt;
+        
+    }
+    public void getCombs(int idx, int n, List<Integer> current) {
+        if (current.size() == 5) {
+            combs.add(new ArrayList<>(current));
+        }
+        
+        for (int i = idx; i <= n; i++) {
+            current.add(i);
+            getCombs(i + 1, n, current);
+            current.remove(current.size() - 1);
+        }
+    }
+    
 }
 /**
-모든 조합에 대해서 q가 가능한지 보면 되는 것.
-모든 조합얻는법? -> 백트래킹..백트래킹?음 잘모르니까 헷갈리네 함 해보자
-백트래킹으로 구현하려면? current에 추가하고 취소하고 이런거잖음.그러면 음
-아 그러면 리턴값을 애초에 모든거로 ㅋ
+비밀코드는 5개의 숫자
+q에 대입해서 통과도미녀 ok
+다통과하면 답 갯수에 ㅜㅊ가
 
+n은 30. 그러몀ㄴ 이제 30개중에 5개 고른게 답후보
+q(시도횟수) 는 10개. 10개의 테스트
+ans는 q에 해당하는 정답개수
 
-*/
+**/
