4b23fe0c7230b21bf5f2d37876728fb5c1ca0d30
양나일, Nile Yang <nileyangdev@gmail.com>
2026-03-07 11:17:10 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -1,18 +1,15 @@
 -- 코드를 작성해주세요
-with recursive gen_table as (
-    select id, 1 as gen
-    from ecoli_data
-    where parent_id is null
-    
-    union all
 
-    select e.id, g.gen + 1
-    from ecoli_data e join gen_table g on e.parent_id = g.id
+with recursive gentable as (
+    select id, 1 as gen
+    from ecoli_data where parent_id is null
+    union all
+    select e.id, gen + 1
+    from gentable g join ecoli_data e on g.id = e.parent_id
 )
 
-
-select count(*) as count, g.gen as generation
-from gen_table g left join ecoli_data e on g.id = e.parent_id
+select count(*) as count, gen as generation
+from gentable g left join ecoli_data e on g.id = e.parent_id
 where e.id is null
-group by generation
-order by generation asc;
+group by gen
+order by gen asc;
