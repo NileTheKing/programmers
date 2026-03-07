2fba52a7e2f5b8c578c019bf12e1e97fa6822894
양나일, Nile Yang <nileyangdev@gmail.com>
2026-03-07 10:17:03 +0900

    [level 1] Title: 특정 형질을 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -1,4 +1,4 @@
 -- 코드를 작성해주세요
 select count(*) as count
 from ecoli_data
-where genotype & 2 = 0 and (genotype & 1 or genotype & 4);
+where genotype & 2 = 0 and (genotype & 1 or genotype & 4); #10 1 100
