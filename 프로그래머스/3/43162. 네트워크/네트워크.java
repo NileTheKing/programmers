4f2194a0cde7aaa08dbae5e367720c9c3a5c21fb
양나일, Nile Yang <nileyangdev@gmail.com>
2026-04-02 15:17:52 +0900

    [level 3] Title: 네트워크, Time: 0.29 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,26 +1,29 @@
 class Solution {
+    boolean[] visited;
     public int solution(int n, int[][] computers) {
-        int answer = 0;
-        boolean[] visited = new boolean[n];
+        int cnt = 0;
+        visited = new boolean[n];
         for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                answer++;
-                dfs(computers, i, visited);
-            }
+            if (visited[i]) continue;
+            cnt++;
+            //System.out.printf("at %d, cluster found.\n", i);
+            dfs(computers, i); //지도정보와 찾고자하는
         }
-        return answer;
+        return cnt;
     }
-    
-    void dfs(int[][] computers, int current, boolean[] visited) {
-        
+    public void dfs(int[][] computers, int current) {
+        if (visited[current]) return;
         visited[current] = true;
+        //System.out.printf("visited %d\n", current);
+        
         for (int i = 0; i < computers.length; i++) {
-            if (computers[current][i] == 1 && !visited[i] && current != i) {
-                dfs(computers, i, visited);
-            }
+            if (current == i) continue;
+            if (computers[current][i] == 0) continue;
+            
+            dfs(computers, i);
         }
     }
 }
 /**
-
+순회하면서 덩어리 찾기. computerse[m][m] = 항상1
 */
