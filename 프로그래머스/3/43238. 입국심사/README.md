8b7021031e06400712a9537f4caa50556e9099fe
양나일, Nile Yang <nileyangdev@gmail.com>
2026-04-03 20:57:06 +0900

    [level 3] Title: 입국심사, Time: 48.53 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?gad_source=1&gad_campaignid=23716289893&gbraid=0AAAAAC_c4nCP1RcmFdn1HxLmPx1B_Q4Pw&gclid=Cj0KCQjwyr3OBhD0ARIsALlo-OncopHkPYOIJ2CEEeTZDYQ2PIj_cUqGjkpVyuFBaLc20UnHja7EjTgaAj8REALw_wcB) 
 
 ### 성능 요약
 
-메모리: 98.8 MB, 시간: 89.82 ms
+메모리: 102 MB, 시간: 48.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 01일 20:28:02
+2026년 04월 03일 20:57:04
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,40 +1,23 @@
 import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
-        long answer = 0;
-        int length = times.length;
-        Arrays.sort(times);
-        long left = 1; //1
-        long right = (long)times[length - 1] * n; // 60
-
-        while(left <= right) {
+        long l = 1;
+        long r = (long)Arrays.stream(times).max().getAsInt() * n;
+        long ans = 0;
+        while (l <= r) {
+            long m = l + (r - l) / 2;//given time
             long done = 0;
-            long mid = left + (right - left) / 2;
-            for (int time : times) {
-                done += mid / time; //mid는 시간. 각 심사관별로 몇명처리가능한지 계싼
+            for (int t : times) {
+                done += m / t;
+                if (done >= n) break;
             }
-            
-            //mid는 예상시간
-            //done은 총 걸린 시간을 mid로 예측했을 때 총 처리한 사람 수
-            //if(done == n) break;
-            if (done >= n) {//실제로 n명보다 더 처리할 수 있었다면 너무 시간을 널널하게줌
-                right = mid - 1;
-                answer = mid;
-            }
-            else {//잘 수행 못헀으므로 시간을 더 줌
-                left = mid + 1;
+            if (done >= n) {
+                ans = m;
+                r = m - 1;
+            }else {
+                l = m + 1;
             }
         }
-        
-        return answer;
+        return ans;
     }
 }
-/**
-구하는 최솟값: 모든 사람이 심사 받는데 걸리는 시간
-
-우선 times 정렬해가지고 가장 짧은애랑 가장 긴 애로 해봄
-mid만들어서 
-
-7과 10의 사이인 8.5라고 가정
-그러면 8.5 
-*/
