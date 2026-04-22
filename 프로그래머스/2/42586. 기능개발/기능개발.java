e868c30a5bbe5ef00163b5833db94411413b81bc
HeadedNowhere <nileyangdev@gmail.com>
2026-04-22 17:59:41 +0900

    [level 2] Title: 기능개발, Time: 2.76 ms, Memory: 88.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,28 +1,42 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> ans = new ArrayList<>();
-        
-        int length = progresses.length;
-        int[] days = new int[length];
-        for (int i = 0; i < length; i++) {
-            days[i] = (int)Math.ceil((double)(100-progresses[i]) / speeds[i]);
+        int n = speeds.length;
+        int[] left = new int[n];
+        for (int i = 0; i < n; i++) {
+            left[i] = (100 - progresses[i] + speeds[i] - 1) / speeds[i];
+            //100 - 93 + 1 - 1 / 1 ->7
+            //100 - 30 + 30 - 1 / 30 -> 3
+            //100 - 55 + 5 - 1 / 5 -> 9
         }
+        //하고싶은것..
+        //7 3 9에..
+        //7 3묶어서 처리 9처리
         
-        //days: {7,3,9}
-        int max = days[0];
+        //[5,10,1,1,20,1]이을
+        //[1,3,2]
+        //
+        int max = left[0];
         int cnt = 1;
-        for (int i = 1; i < days.length; i++) {
-            if (days[i] <= max) {//한번에 배포가능
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 1; i < n; i++) {
+            if (left[i] <= max) {
                 cnt++;
-            }else {//이젠 안된다. 처리
+            }else {
+                max = left[i];
                 ans.add(cnt);
-                cnt = 1;//초기화
-                max = days[i];
+                cnt = 1;
             }
         }
         ans.add(cnt);
-        
-        return ans.stream().mapToInt(i->i).toArray();
+        return ans.stream().mapToInt(i -> i).toArray();
     }
 }
+/**
+음..순서가있따.
+약간 뭐 그 스택이나 어디에쌓아두고..모노스택?암튼
+7 3 9를 순회
+7가지고 계속 순회 언제까지? 더 큰거나오면 멈춤
+더작은거있으면.. 얘가 병목이라
+
+*/
