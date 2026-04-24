511823374871e6a196a8b5a40b8f266111054a71
HeadedNowhere <nileyangdev@gmail.com>
2026-04-24 13:41:32 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 4.10 ms, Memory: 93 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,62 +1,54 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] jobs) {
-        
-        int[][] job = new int[jobs.length][3];//소요시간, 요청시각, 작업번호
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> {
-            if (a[0] != b[0]) {
-                return Integer.compare(a[0], b[0]);
+        int[][] newJobs = new int[jobs.length][3];//번호 시점 시간
+        PriorityQueue<int[]> pq = new PriorityQueue<>( //번호, 요청시점, 소요시간
+            (o1, o2) -> {
+                if (o1[2] == o2[2]) {
+                    if (o1[1] == o2[1]) return o1[0] - o2[0];
+                    else return o1[1] - o2[1];
+                }else {
+                    return o1[2] - o2[2];
+                }
             }
-            else if (a[1] != b[1]) {
-                return Integer.compare(a[1], b[1]);
-            }
-            else {
-                return Integer.compare(a[2], b[2]);
-            }
-        });
-        
-        //cf: jobs {요청시각, 소요ㅕ시긴}
-        //job : 소요시간, 요청시각, 작업번호
-        for (int i = 0; i < jobs.length; i++) {
-            job[i][0] = jobs[i][1]; //소요시간 작앞
-            job[i][1] = jobs[i][0]; //요청시간 작앞
-            job[i][2] = i; //작업번호 작앞
+        );
+        for (int i = 0;i < jobs.length; i++) {
+            newJobs[i][0] = i;
+            newJobs[i][1] = jobs[i][0];
+            newJobs[i][2] = jobs[i][1];
         }
-        Arrays.sort(job, (a, b) -> Integer.compare(a[1], b[1]));
+        Arrays.sort(newJobs, (a,b) -> a[1] - b[1]);//시점기준 오름차순정렬
         
-        
-        //pq에서 작업하나씩 꺼내면서 반환시간을 구함.
-        //반환시간 = 현재시간 - 요청시간
-        //현재시간 = 이전작업 종료시간 + 빈시간 + 소요시간
-        //빈시간 = 이전 종료시간 - 수행중작업 요청시간
-        //current : {소 요 작번}
-        int time = 0;
-        int prev = 0;
-        int sum = 0;
+        int totalWait = 0;
+        int done = 0;
         int idx = 0;
+        int targetDone = jobs.length;
+        int time = 0;
+        while (done < targetDone) {
+            while (idx < jobs.length && newJobs[idx][1] <= time) {
+                pq.offer(newJobs[idx++]);
+            }
+            
+            //다음으로 이동... 큐가비엉있으면 다음작업으로 시간이동
+            if (pq.isEmpty()) {
+                //비어있으면 다음놈으로 시간이동
+                time = newJobs[idx][1];
+            }else { //차있으면 작업처리..: poll, 시간, 계산
+                int[] polled = pq.poll();//
+                done++;
+                time += polled[2];// 시간은 이미 처리되어있으니까 소요시간
+                totalWait += (time - polled[1]);
+            }
         
-        while (!pq.isEmpty() || idx < job.length) {
-            //대기목록 추가 및 처리
-            while (idx < job.length && job[idx][1] <= time) {
-                pq.offer(job[idx]);
-                idx++; //요청시간별로 정렬한 job배열을 idx로 추적
-            }
-            
-            if (!pq.isEmpty()) {
-                int[] current = pq.poll();
-                time += current[0];
-                sum = sum + time - current[1];
-            }
-            else {
-                time = job[idx][1];
-            }
-            
         }
+        return totalWait / jobs.length;
         
-        
-        return sum/jobs.length;
     }
 }
-/**
-pq에 넣어놓고 순서대로 비교해서 앞에서 처리.. 그냥 계산만 하면 됨
-*/
+        //내가  하고싶은것
+        //q돌면서 시간시뮬레이션 및 다음 작업 찾으면서 ..idx는 다음작업용이었던거 같다.
+        //정렬은 필요없다치고.. 정렬안하면 그냥 매번 풀스캔하면되잖어. 일단 해야하나?
+        //시간복잡도 jobs500이잖어.
+        //힙자체가 nlogn이고 매번 풀스캔하면 n이니까 n^2logn인데..
+        //n이 500이니까 충분
+        
