9ee7b1391f95a95c2261d2b9a3b771fa68886036
HeadedNowhere <nileyangdev@gmail.com>
2026-04-25 00:27:10 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 19.16 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -1,33 +1,28 @@
 class Solution {
     public int[] solution(int[] sequence, int k) {
-        int[] ans = new int[2];
-        int sum = 0;
+        int[] ans = new int[2]; //시작,끝인덱스
+        long sum = 0;
         int l = 0;
-        int min = Integer.MAX_VALUE;
+        int min = sequence.length + 1;
         for (int i = 0; i < sequence.length; i++) {
-            
+            //i이자 r은 오른쪽으로 계쏙간다.
+            // System.out.printf("===start of %d===\n", i);
             int r = i;
-            //System.out.printf("====[%d,%d]====\n", l,r);
-            sum += sequence[r];
-            //System.out.printf("[%d,%d] sum= %d\n", l,r, sum);
-            while (sum > k && l < r) { //같
+            sum += sequence[i];
+            //l이 너무 왼쪽에 있으면 크니까 안됨.
+            while (l < r && sum > k) {
                 sum -= sequence[l++];
             }
-            //System.out.printf("after [%d, %d] sum= %d\n", l,r,sum);
-            if (sum == k) {
-                //길이가 더 짧으면 덮ㅅ어쓰기
-                if (r - l + 1 < min) {
-                    //System.out.printf("{%d,%d} set\n", l,r);
-                    ans[0] = l;
-                    ans[1] = r;
-                    min = r - l + 1;
-                }//아닌데 더 뒤에 나온거면 컷
+            // System.out.printf("l,r,sum : %d, %d, %d\n", l, r, sum);
+            //지금은 l부터 r까지합임.
+            if (sum == k && r - l + 1 < min) {
+                ans[0] = l;
+                ans[1] = r;
+                min = r - l + 1;
+                // System.out.printf("l,r,min\n", l, r, min);
             }
-            //System.out.printf("====end [%d,%d]====\n", l,r);
+            // System.out.printf("===end of %d===\n", i);
         }
         return ans;
     }
 }
-/**
-sliding window.
-*/
