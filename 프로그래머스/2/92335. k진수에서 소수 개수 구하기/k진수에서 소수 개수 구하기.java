88b03dd69d5c1bc3b40ed2d711a669b1e6db8d1a
HeadedNowhere <nileyangdev@gmail.com>
2026-04-28 20:46:38 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 12.22 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,37 +1,43 @@
+import java.util.*;
 class Solution {
     public int solution(int n, int k) {
-        String kary = convert(k, n);
-        String[] splited = kary.split("0");
+        String tran = n_nary(n, k);
+        String[] parts = String.valueOf(tran).split("0");
         int cnt = 0;
-        for (String part : splited) {
-            if (part.length() > 0 && isPrime(Long.parseLong(part))) {
-                //System.out.printf("%s counts\n", part);    
-                cnt++;
-            }
+        for (String p : parts) {
+            //p가 소수인지
+            if (p.length() == 0) continue;
+            if (isPrime(p)) cnt++;
         }
         return cnt;
-        
     }
-    public String convert(int k, int number) {
-        if (number == 0) return "0";
-        StringBuilder result = new StringBuilder();
-        
-        while (number > 0) {
-            result.append(number % k);
-            number /= k;
+    public String n_nary(int n, int k) { 
+        //n을 k진수로. 예를들어 2진수는 어케나오냐면..
+        //30은 2진수로바꿀떄 -> 몫15 나머지0 --나누기-- 몫7 나머지1 --나누기-- 목3 나머지1 --나누기2-- 몫1 나머지1 --- 몫0 나머지 1-- 끝! 나눠지는애가 0이므로
+        //근데 숫자를 어케 char로하지?
+        StringBuilder sb = new StringBuilder();
+        while (n > 0) {
+            int quotient = n / k;
+            int remainder = n % k;
+            char c = (char)(remainder + '0');
+            sb.append(c);
+            n = quotient;
         }
-        return result.reverse().toString();
+        sb.reverse();
+        return sb.toString();
     }
-    public boolean isPrime(long number) {
-        if (number < 2) return false;
-        boolean isAvailable = true;
-        for (long i = 2; i * i <= number; i++) {
-            if (number % i == 0) return false;
+    public boolean isPrime(String str) {
+        long num = Long.parseLong(str);
+        if (num == 1) return false;
+        
+        //num가지고 소수확인.. 
+        for (long i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
         }
         return true;
     }
 }
 /**
-n진수 변경 코드
-String으로 받아서 split
+1.변환 2.카운트. 카운트는 그냥 0으로 자르는거아님?split
+변환은 쉽지.
 */
