a21abc7ba7f84304fce94cbc58848fa765bff765
HeadedNowhere <nileyangdev@gmail.com>
2026-04-28 21:17:05 +0900

    [level 2] Title: 괄호 회전하기, Time: 40.37 ms, Memory: 96.1 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/README.md
+++ 프로그래머스/2/76502. 괄호 회전하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 회전하기 - 76502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502) 
 
 ### 성능 요약
 
-메모리: 84.5 MB, 시간: 65.04 ms
+메모리: 96.1 MB, 시간: 40.37 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 29일 13:11:13
+2026년 04월 28일 21:17:01
 
 ### 문제 설명
 
--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,43 +1,57 @@
 import java.util.*;
 class Solution {
     public int solution(String s) {
-        Map<Character, Character> map = new HashMap<>();
-        Set<Character> open = new HashSet<>();
-        map.put(')', '(');
-        map.put(']', '[');
-        map.put('}', '{');
-        for (char c : map.values()) {
-            //System.out.printf("%c added\n", c);
-            open.add(c);
-        }
+        int len = s.length();
         int cnt = 0;
-        for (int i = 0; i < s.length(); i++) { //각 시작점
-            //끝까지 순회
-            //새로운스택
+        Outter : for(int i = 0; i < len; i++) { //i칸왼쪽으로했을떄 
+            //시작인덱스가 i라는거임
+            int start = i;
             Stack<Character> stk = new Stack<>();
-            boolean available = true;
-            for (int j = 0; j < s.length(); j++) {
-                int current = (i + j) % s.length();// 012345이 있고 2부터시작이면 234501.
-                char c = s.charAt(current);
-                if (open.contains(c)) {//지ㅐ금 넣는게 여는괄호
+            for (int j = 0; j < len; j++) {//횟수임.다읽어야함
+                int currentIdx = (start + j) % len;
+                char c = s.charAt(currentIdx);
+                if (isOpening(c)) {
                     stk.push(c);
-                }else { //닫는괄호
-                    if (stk.isEmpty()) {
-                        available = false;
-                        break; //불가능
-                    }
-                    char top = stk.peek();
-                    if (top == map.get(c)) { //짝맞음
+                }else {//한번에하나보는거임. 연쇄폭발없음.
+                    if (!stk.isEmpty() && matches(stk.peek(), c)) {
                         stk.pop();
-                    }else {//짝안맞음
-                        available = false;
-                        break;
+                    }else {
+                        //아예안됨..포기.
+                        continue Outter;//아예 다음 경우
                     }
                 }
             }
-            //플래그가 가능하면 카운트늘리기
-            if (available && stk.isEmpty()) cnt++;
+            if (stk.isEmpty()) cnt++;// 다비어야함.
         }
         return cnt;
     }
+    public boolean isOpening(char c) {
+        if (c == '(' || c == '[' || c == '{') return true;
+        else return false;
+    }
+    public boolean matches(char c1, char c2) {
+        if (c1 == '(') {
+            if (c2 == ')') return true;
+        }else if (c1 == '[') {
+            if (c2 == ']') return true;
+        }else {
+            if (c2 == '}') return true;
+        }
+        return false;
+    }
 }
+/**
+구현 브루트포스. 나머지쓰면되곘네 이건 회전이니.
+그러면 이거를 음.. 미리만들어놓을필요도 딱히없음그냥 읽으면되지..근데
+효율적으로 가능한가? 
+회전할떄마다 시작칸을 하고 길이는 고정이고 매번 나머지를 써가지고 다음칸보면됨..
+맞네. 그러면 그떄마다 스택쓰면됨
+괄호..음 ㅇ거 스택인데 머릿속으로 자동이아니라 내가 머리써봐야함
+여는거 들어와
+닿는거야? 그럼 top이랑 맞아야함.
+여는거넣어
+
+ㅇㅋ
+아 반직고나적인거 찾았음. 이거 그 연쇄작용떄문인데 택배큐 그거는넣을때마다 연쇄폭발시키는거고
+이거는 안되면 걍 안되는거임.
+*/
