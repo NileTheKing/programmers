97e80e4b8ce94dffdfa7df15875090c8d3415eeb
HeadedNowhere <nileyangdev@gmail.com>
2026-04-29 20:22:08 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 85.19 ms, Memory: 223 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -1,27 +1,22 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] numbers) {
+        Stack<Integer> stack = new Stack<>();//인덱스
+        //stack.push(numbers[0]);
         int[] ans = new int[numbers.length];
-        Arrays.fill(ans, -1);
-        Stack<Integer> mono = new Stack<>();//인덱스정보 담은 모노스택
-        mono.add(0);
-        int i= 1;
-        while (!mono.isEmpty() && i < numbers.length) {
-
-            while (!mono.isEmpty() && numbers[mono.peek()] <  numbers[i]) {
-                //System.out.printf("debug\n");
-                int poppedIdx = mono.pop();
-                ans[poppedIdx] = numbers[i];// 꺼낸거는 다 해결해주기
-            }
-            mono.push(i);
-            i++;
-            
+        Arrays.fill(ans, -1);//초기값
+        for (int i = 0; i < numbers.length; i++) {
+            while (!stack.isEmpty() && numbers[stack.peek()] < numbers[i]) {
+                //i가 계속 크면 갱신
+                int poppedIdx = stack.pop();
+                ans[poppedIdx] = numbers[i]; //답은 숫자..idx넣는이유는 ans때문
+            }//다 처리되면 이제 stack은 더큰값이 있고 ..제일작은 number[i].
+            stack.push(i);
         }
         return ans;
-        
     }
 }
 /**
-
-
+단조스택이네.
+10^7이므로 O(N)이 맥스임.
 */
