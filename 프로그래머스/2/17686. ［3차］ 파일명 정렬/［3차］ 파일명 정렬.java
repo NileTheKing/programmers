f10c62b48be410d21302bf80762ae8ea34af466a
HeadedNowhere <nileyangdev@gmail.com>
2026-05-07 00:44:59 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 18.62 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,63 +1,53 @@
 import java.util.*;
 class Solution {
     public String[] solution(String[] files) {
-        
         Arrays.sort(files, (o1, o2) -> {
-            String[] parts1 = seperate(o1);
-            String[] parts2 = seperate(o2);
-            
-            int head = compareHead(parts1[0], parts2[0]);
-            int number = compareNumber(parts1[1], parts2[1]);
-            
-            if (head != 0) {
-                return head;
-            }else if(number != 0) {
-                return number;
-            }else {
-                return 0;
+            String[] parts1 = transform(o1);
+            String[] parts2 = transform(o2);
+            String head1 = parts1[0].toLowerCase();
+            String head2 = parts2[0].toLowerCase();
+            int number1 = Integer.parseInt(parts1[1]);
+            int number2 = Integer.parseInt(parts2[1]);
+            String tail1 = parts1[2];
+            String tail2 = parts2[2];
+            //head는 사전순.. compareToignoreCase
+            if (head1.compareTo(head2) != 0) {
+                return head1.compareTo(head2); //순서앞
+            }else if (number1 != number2) { //숫자앞
+                return number1 - number2;
+            }else { 
+                return 0;//들어온순. << 이게 들어온순이어야하는데...항상 
             }
-            
+            //head같으면 숫자.. 숫자이넫             
         });
         return files;
     }
-    
-    public String[] seperate(String s) {
-        
-        String[] parts = new String[3];
-        int length = s.length();
-        //head는 숫자나오기전 문자
+    public String[] transform(String str) {//0 1 2로 만들어서 준다.
+        String[] res = new String[3];
+        //number찾는다
+        //number 시작 number끝
+        //일단 시작
         int start = 0;
-        int end = 1; //최소1글자
-        while (end < length-1 && !Character.isDigit(s.charAt(end))) {
-            end++;
-        }            
-        parts[0] = s.substring(start,end);//end가 안될떈 어차피 그거 빠짐
-        //number는 연속된 숫자.최대5.
-        //숫자인것들은 최대5개까지 세어버린다.
-        start = end;
-        while (end < length && Character.isDigit(s.charAt(end)) && (end-start + 1) <= 5)
-        {
-            //참이라면 계속 늘림
-            end++;
-            //근데 너무 멀리왔디(5) 그럼 멈춘다.
+        while (start < str.length() && !(str.charAt(start) - '0' >= 0 && str.charAt(start) -'0' <= 9)) {
+            start++;
         }
-        parts[1] = s.substring(start, end);        
-        //tail은 나머지
-        parts[2] = s.substring(end, length);
+        //start부터 숫자임.
+        //이제 최대 4번 이동할떄까지..한정 숫자면 계속.. 그니까 end - start가 4까진 ㅇㅋ
+        int end = start + 1;
+        //안될떄까지 옮길거임. 거리가 5이든가 숫자가 아니든가. 그러면 end -1] 까지가 숫자임
+        while (true) { //조건복잡하니까 내부로 옮김
+            if (end >= str.length() || end - start >= 5 || 
+                str.charAt(end) - '0' < 0 || str.charAt(end) - '0' > 9 ) break;
+            end++;
+        }
+        //이제 substring start,end하면 딱맞음.
+        res[0] = str.substring(0, start); //head
+        res[1] = str.substring(start, end); //number
+        res[2] = str.substring(end, str.length());//tail
+        //System.out.printf("===start %s===\n", str);
+        //for (String r : res) System.out.printf("%s ", r);
+        //System.out.printf("\n===end %s===\n",str);
+        return res;
         
-        // System.out.println("printing " + s);
-        // for (String p : parts) {
-        //     System.out.printf("%s ``", p);
-        // }
-        System.out.println();
-        return parts;
-    }
-    public int compareHead(String s1, String s2) {
-        String lower1 = s1.toLowerCase();
-        String lower2 = s2.toLowerCase();
-        return lower1.compareTo(lower2);
-    }
-    public int compareNumber(String s1, String s2) {
-        return Integer.parseInt(s1) - Integer.parseInt(s2);
     }
 }
