eb56fac90d63e183b106b4a867e02c1b452f93c1
HeadedNowhere <nileyangdev@gmail.com>
2026-05-09 13:44:54 +0900

    [level 2] Title: 택배상자, Time: 34.30 ms, Memory: 146 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -1,18 +1,31 @@
 import java.util.*;
 class Solution {
     public int solution(int[] order) {
-        Deque<Integer> stack = new ArrayDeque<>();
-        int targetIdx = 0;
-        int cnt = 0;
-        //목표 타겟이 아닌 들어오는 박스에 초점을 둔다.
-        for (int i = 1; i <= order.length; i++) { //박스가 온다
-            stack.offerFirst(i);//일단넣기
-            while (!stack.isEmpty() && stack.peek () == order[targetIdx]) {
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        //초점을..들어오는 상자
+        int len = order.length;
+        int idx = 0; //order처리여부..queue대신
+        for (int i = 1; i <= len; i++) {
+            //일단스택넣기..똑같음.
+            stack.offerFirst(i);
+            while (idx < len && !stack.isEmpty() && order[idx] == stack.peekFirst()) {
                 stack.pollFirst();
-                targetIdx++;
-                cnt++;
+                idx++;
             }
         }
-        return cnt;
+        return idx;
+        // 4 3 1 2 5
+        // 1스택에넣기 -> 1
+        // 2스택에 넣기 -> 2 1
+        //3 스택에넣기 -> 3 2 1
+        //4스택에 넣기.. 어? 마즈니까 4.. 어 또 맞으니까 3.. 스택 2 1
+        //5 스택에넣기..어 안맞네..끝.인데 종료해줘야함? 그러면 코드가 어떻게되지
     }
 }
+/**
+컨베이어 입구[    ] 큐
+보조벨트 입구 [ ) 스택.
+종료조건은 원하는순서대로 못한다면..
+종료
+
+*/
