8a9728cbd0bc241e34de5d295861aa1d9a5875a7
HeadedNowhere <nileyangdev@gmail.com>
2026-05-22 23:01:56 +0900

    [level 2] Title: 기능개발, Time: 1.93 ms, Memory: 61.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,42 +1,33 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int n = speeds.length;
-        int[] left = new int[n];
-        for (int i = 0; i < n; i++) {
-            left[i] = (100 - progresses[i] + speeds[i] - 1) / speeds[i];
-            //100 - 93 + 1 - 1 / 1 ->7
-            //100 - 30 + 30 - 1 / 30 -> 3
-            //100 - 55 + 5 - 1 / 5 -> 9
-        }
-        //하고싶은것..
-        //7 3 9에..
-        //7 3묶어서 처리 9처리
-        
-        //[5,10,1,1,20,1]이을
-        //[1,3,2]
-        //
-        int max = left[0];
-        int cnt = 1;
         List<Integer> ans = new ArrayList<>();
-        for (int i = 1; i < n; i++) {
-            if (left[i] <= max) {
+        int[] left = new int[speeds.length];
+        for (int i = 0; i < speeds.length; i++) {
+            left[i] = (100 - progresses[i] + speeds[i] - 1) / speeds[i];
+            // System.out.println(left[i]);
+        }
+        int prev = left[0];
+        int cnt = 1;
+        for (int i = 1; i < left.length; i++) {
+            if (left[i] <= prev) {
                 cnt++;
-            }else {
-                max = left[i];
+            }else { //싹다 추가
                 ans.add(cnt);
                 cnt = 1;
+                prev = left[i];
             }
         }
         ans.add(cnt);
-        return ans.stream().mapToInt(i -> i).toArray();
+        return ans.stream().mapToInt(i->i).toArray();
     }
 }
 /**
-음..순서가있따.
-약간 뭐 그 스택이나 어디에쌓아두고..모노스택?암튼
-7 3 9를 순회
-7가지고 계속 순회 언제까지? 더 큰거나오면 멈춤
-더작은거있으면.. 얘가 병목이라
+a / b 하면 올림안되니께
+a + b - 1 / b
+7 3 9
+인데.. 같거나작으면 계속 묶음
+5 10 1 1 20 1
+1 3 2
 
 */
