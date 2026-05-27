813a9909459c068143ae78e77412bf9836ad7887
HeadedNowhere <nileyangdev@gmail.com>
2026-05-27 12:23:21 +0900

    [level 3] Title: 기지국 설치, Time: 0.36 ms, Memory: 57.3 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,31 +1,23 @@
-import java.util.*;
 class Solution {
     public int solution(int n, int[] stations, int w) {
-        //stations 심지어 정렬이다.. ㅋ
-        //일단 앞에 되게끔하고
         int coverWidth = 2 * w + 1;
+        int lastCovered = 0;
         int cnt = 0;
-        int lastCover = 0;
         for (int s : stations) {
-            //System.out.printf("===start APT%d===\n", s);
-            int coverFrom = s - w;//번 아파트부터 커버..이거 1index임
-            //lastcover 다음부터 covefrom -1까지 숫자센다음에 계산
-            //1 2
-            int numApt = coverFrom - lastCover - 1;
-            cnt += ((numApt + coverWidth - 1) / coverWidth);
-            //2 + 3 - 1 / 3
-            lastCover = s + w;
-            //System.out.printf("put %d\n",((numApt + coverWidth - 1) / coverWidth));
-            //System.out.printf("===end APT%d===\n", s);
+            int firstCovered = s - w;
+            int dist = firstCovered - lastCovered - 1; //3 - 0 - 1 -> 2
+            // System.out.printf("s %d,last %d start %d dist %d, cnt %d\n", s, lastCovered, firstCovered, dist, (dist + coverWidth - 1) / coverWidth);
+            cnt += (dist + coverWidth - 1)  / coverWidth;
+            //2개 커버하려면 1개면 됨.
+            // 2 + 3 - 1 / 3 -> 1
+            lastCovered = s + w; 
         }
-        int numApt = n - lastCover;
-        cnt += ((numApt + coverWidth - 1) / coverWidth);
+        //N번아파트에서 마지막커버도 더해야한다
+        // System.out.printf("before last apt cnt: %d\n", cnt);
+        // System.out.printf("(n:%d) (lastCovered:%d)\n", n, lastCovered);
+        cnt += (n + 1 - lastCovered - 1 + coverWidth - 1) / coverWidth;
+        // System.out.printf("after last apt cnt: %d\n", cnt);
+        
         return cnt;
     }
 }
-/**
-N 2억..O(N)하거나 O(LOGN).
-이제는 그냥 보이네이런류는 그냥.. 그 어차피해야하니까 그리디인거지..
-1 2에 몇개필요? 1개
-
-*/
