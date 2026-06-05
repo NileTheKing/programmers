74da48d21be5a7b09f99a99d96f40cdf132a61cb
HeadedNowhere <nileyangdev@gmail.com>
2026-06-05 13:13:27 +0900

    [level 2] Title: 기능개발, Time: 3.04 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,17 +2,16 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         List<Integer> ans = new ArrayList<>();
-        int[] left = new int[speeds.length];
-        for (int i = 0; i < speeds.length; i++) {
+        int length = progresses.length;
+        int[] left = new int[length];
+        for (int i = 0; i < length; i++) {
             left[i] = (100 - progresses[i] + speeds[i] - 1) / speeds[i];
-            // System.out.println(left[i]);
         }
         int prev = left[0];
         int cnt = 1;
-        for (int i = 1; i < left.length; i++) {
-            if (left[i] <= prev) {
-                cnt++;
-            }else { //싹다 추가
+        for (int i = 1; i < length; i++) {
+            if (left[i] <= prev) cnt++;
+            else {
                 ans.add(cnt);
                 cnt = 1;
                 prev = left[i];
@@ -22,12 +21,3 @@ class Solution {
         return ans.stream().mapToInt(i->i).toArray();
     }
 }
-/**
-a / b 하면 올림안되니께
-a + b - 1 / b
-7 3 9
-인데.. 같거나작으면 계속 묶음
-5 10 1 1 20 1
-1 3 2
-
-*/
