ca29ee94a2922539bd21c5784d1708745f2af317
HeadedNowhere <nileyangdev@gmail.com>
2026-06-05 11:58:08 +0900

    [level 3] Title: 2차원 동전 뒤집기, Time: 9.95 ms, Memory: 64.7 MB -BaekjoonHub

--- 프로그래머스/3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.java
+++ 프로그래머스/3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.java
@@ -1,63 +1,50 @@
-import java.util.*;
 class Solution {
+    int min = Integer.MAX_VALUE;
     public int solution(int[][] beginning, int[][] target) {
-        int m = beginning.length;
-        int n = beginning[0].length;
-        int min = Integer.MAX_VALUE;
-        // 1 * 2^10 = 1024..
-        for (int r = 0; r < (1 << m); r++) {
-            Outter : for (int c = 0; c < (1 << n); c++) {
-                //r은 행비트 켜짐 c는 열비트 켜짐(뒤집어짐)..1뒤집기 0안뒤집힌상태(초기상태)
-                //r,c상태가지고 테이블풀스캔
-                // System.out.printf("===start bit: (%d, %d)===\n",r,c);
-                for (int i = 0; i < m; i++) {
-                    for (int j = 0; j < n; j++) {
-                        int start = beginning[i][j];
-                        boolean switched = (((1 << i) & r) != 0) ^ (((1 << j) & c) != 0);
-                        //true면 바꿈
-                        //보완한거는 r c 둘다1이거나 둘다0이면 바뀜.
-                        //둘중하나만 1이면 바뀜..xor
-                        if (switched) start ^= 1;
-                        // System.out.printf("at row %d flipped?: %b\n", i, ((1 << i) & r) != 0);
-                        // System.out.printf("at col %d flipped?: %b\n", i, ((1 << i) & c) != 0);
+        int m = beginning.length; //행(세로)
+        int n = beginning[0].length;//가로
+        for (int i = 0; i < (1 << m); i++) {//0부터 1023까지. 1<<10은 1024니까..우리는 11111 11111까지. 
+            Outter : for (int j = 0; j < (1 << n); j++) {
+                //이제 행뒤집은 스위치, 열 뒤집은 스위치 상태있음.
+                //beginning 기준 스위치 상태보고 뒤집어가지고 target이랑 비교. 다르면 중지
+                //여기서.. 스위치가 01 10이면 뒤집는거고 00 11이면 안뒤집음..이거는 xor일듯
+                //왜냐면 xor은..or인데 exclusive해야하니까.
+                for (int k = 0; k < m; k++) {
+                    for (int l = 0; l < n; l++) {
+                        int before = beginning[k][l];
+                        //현재 k(행) 뒤집힌 여부랑 l뒤집현여부알아야한다.
+                        //소스는 i와 j인데 어케아냐? i의 k번째 비트와 j의l번쨰비트
+                        int rowBit = (i >> k) & 1;
+                        int colBit = (j >> l) & 1;
+                        int after = before ^ rowBit ^ colBit;
                         
-                        // System.out.printf("at (%d,%d) values:(%d %d)\n",i,j,start, target[i][j]);
-                        if (start != target[i][j]) continue Outter;
+                        if (after != target[k][l]) continue Outter;
                     }
-                }//무사히 종료.r이랑 c 뒤집은거(1)몇개인지 카운트
-                int cnt = 0;
-                for (int i = 0; i < m; i++) {
-                    if (((1 << i) & r) != 0) cnt++;
                 }
-                for (int i = 0; i < n; i++) {
-                    if (((1 << i) & c) != 0) cnt++;
+                //같으면 카운트 업데이트..카운트는? 스위치(i,j) 1갯수 세면 된다.
+                int cnt1 = 0, cnt2 = 0;
+                //i와 j의 비트갯수를 센다. 1갯수.
+                // System.out.printf("========\n");
+                for (int x = 0; x < m; x++) {
+                    if (((i >> x) & 1) == 1) 
+                    {    
+                        cnt1++;
+                        // System.out.printf("행 %d 뒤집음\n", x + 1);
+                    }
                 }
-                min = Math.min(min, cnt);
-                // System.out.printf("===end bit: (%d, %d)===\n",r,c);
+                
+                for (int x = 0; x < n; x++) {
+                    if (((j >> x) & 1) == 1) 
+                    {    
+                        cnt2++;
+                        // System.out.printf("행 %d 뒤집음\n", x + 1);
+                    }
+                }
+                // System.out.printf("i j cnt1 cnt2: %d %d %d %d\n", i,j,cnt1, cnt2);
+                min = Math.min(min, cnt1 + cnt2);
             }
+            
         }
         return min == Integer.MAX_VALUE ? -1 : min;
     }
 }
-/**
-자 몇가지 결론을 내야한다
-언제 뒤집냐? 음... 일단 게임을 자연스럽게 이해해야함
- 2 4행을 뒤집은이유가 뭘까? 음...
- 뒤집는 다는 거는 한 줄을 아예 뒤집는 다는 것이고..
- 음..무슨 뭐 그리디같은게 있나?
-시간복잡도 한 번 봐보자.
-가로세로 10 10.. 근데 그렇다면.. 그냥 완탐 떄려버리면 무조건 구할 ㅅ ㅜ있지.
-모든 경우의수(몇번행 뒤집었는지 몇번열 뒤집었는지) 모든 조합을 다 구할수있어
-어떻게? 총 20비트있으면 가능. 20비트면 충분.. 백만되나.
-0-9 10-19
-그상태로 다 뒤집어졌는지.
-이게 아니라면 백트래킹 완탐인데 매번 경우의수가 100개있음.. 그래서 안되나?
-근데 백트래킹이나 비트마스킹 완탐이나 똑같아야하는거 아닌가?
-아 근데 이게 순서가 관련이 없네
-그래서 백트래킹으로 하면 상태를 들고다니는 거고
-근데 그래도 100개의 경우의수가 있는거아니냐? 목표비트를 뭐 들고다니면서 할수도없고
-그냥
-최종상태에서 매번 검사하는게 낫지않음?
-경우의수 백만개고.. 칸100개니까 딱 1억.
-
-*/
