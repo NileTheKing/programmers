0cf6ba0241177ad0cd10244a0a9fbed294efcdc6
HeadedNowhere <nileyangdev@gmail.com>
2026-06-26 14:50:18 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 7.80 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -1,86 +1,89 @@
 import java.util.*;
 class Solution {
-    char[][] grid;
-    int m;
-    int n;
+    int m, n;
     public int solution(int m, int n, String[] board) {
-        //char[][] 변환
         this.m = m;
         this.n = n;
-        grid = new char[m][n];
+        //char[][] 로 바꾸기
+        char[][] grid = new char[m][n];
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) grid[i][j] = board[i].charAt(j);
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = board[i].charAt(j);
+            }
         }
-        
-        int cnt = 0;
+        //변한게없으면 그만두기
+        int ans = 0;
         while (true) {
-            int currentCnt = 0;
-            List<int[]> blocks = new ArrayList<>();
-            //지울 블록 찾기
-            findBlocks(blocks);
-            //블록 지우기
-            currentCnt += deleteBlocks(blocks);
-            //종료조건
-            if (currentCnt == 0) return cnt;
+            //스캔 후 4덩어리찾기
+            List<int[]> candidates = getCandidates(grid);
+            //찾은거 처리. 처리한거는 $로 표시해서 중복잘 뺴기
+            int cnt = 0;
+            for (int[] candi : candidates) {
+                int r = candi[0];
+                int c = candi[1];
+                int debugcnt = 0;
+                if (grid[r][c] != '$') {
+                    grid[r][c] = '$';
+                    cnt++;
+                }
+                if (grid[r + 1][c] != '$') {
+                    cnt++;
+                    grid[r+1][c] = '$';
+                }
+                if (grid[r + 1][c + 1] != '$') {
+                    cnt++;
+                    grid[r+1][c+1] = '$';
+                }
+                if (grid[r][ c + 1] != '$') {
+                    cnt++;
+                    grid[r][c+1] = '$';
+                }
+            }
+            ans += cnt;
+            if (cnt == 0) return ans;
+            //떨어뜨리기
+            drop(grid);
             
-            //다음준비
-            cnt += currentCnt;
-            drop();
         }
     }
-    public boolean isBlock(int r, int c) {
-        if (grid[r][c] =='.') return false;
-        char friend = grid[r][c];
-        if (grid[r][c + 1] != friend) return false; 
-        if (grid[r + 1][c] != friend) return false;
-        if (grid[r + 1][c + 1] != friend) return false;
-        
+    public List<int[]> getCandidates(char[][] grid) {
+        List<int[]> res = new ArrayList<>();
+        for (int i = 0; i < m -1; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                if (grid[i][j] == '$') continue;
+                if (!isSame(grid, i, j)) continue;
+                res.add(new int[] {i, j});
+            }
+        }
+        return res;
+    }
+    public boolean isSame(char[][] grid, int r, int c) {
+        char origin = grid[r][c];
+        if (grid[r + 1][c] != origin) return false;
+        if (grid[r][c + 1] != origin) return false;
+        if (grid[r + 1][c + 1] != origin) return false;
         return true;
     }
-    public void findBlocks(List<int[]> blocks) {
-        for (int i = 0; i < m - 1; i++) {
-            for (int j = 0;j < n - 1; j++) {
-                if (isBlock(i,j)) {
-                    blocks.add(new int[] {i,j});
-                }
+    public void drop(char[][] grid) {
+        for (int c = 0; c < n; c++) {
+            StringBuilder sb = new StringBuilder();
+            for (int r = m - 1; r >= 0; r--) {
+                if (grid[r][c] != '$') sb.append(grid[r][c]);
+                grid[r][c] = '$';
             }
-        }
-    }
-    public int deleteBlocks(List<int[]> blocks) {
-        int currentCnt = 0;
-        for (int[] b : blocks) {
-            if (grid[b[0]][b[1]] !='.') {
-                grid[b[0]][b[1]] = '.'; 
-                currentCnt++;
-            }
-            if (grid[b[0]][b[1] + 1] != '.') {
-                currentCnt++;
-                grid[b[0]][b[1] + 1] = '.';
-            }
-            if (grid[b[0] + 1][b[1]] != '.'){
-                currentCnt++;
-                grid[b[0] + 1][b[1]] = '.';
-            }
-            if (grid[b[0] + 1][b[1] + 1] != '.') {
-                grid[b[0] + 1][b[1] + 1] = '.';
-                currentCnt++;
-            }
-        }
-        return currentCnt;
-    }
-    public void drop() {
-        for(int i = m - 1;i >= 1; i--) {//바닥행
-            for (int j = 0; j < n; j++) {//열
-                if (grid[i][j] == '.') {//본인이 비었으면
-                    Outter : for (int k = i; k  >= 0; k--) {
-                        if (grid[k][j] != '.') {      
-                            grid[i][j] = grid[k][j];
-                            grid[k][j] = '.';//얘도 떨어져야함.. .으로
-                                break Outter;
-                        }
-                    }
-                }
+            //이제 sb읽어서 아래부터 다시 채워넣는다.
+            for (int i = 0; i < sb.length(); i++) {
+                grid[m - 1 - i][c] = sb.charAt(i);
             }
         }
     }
 }
+/**
+턴별로 스캔
+    4덩어리 찾기: 좌측상단기준 4개찾기. 겹치는거..
+        중복제거!
+    제거: 공백으로하고 갯수세기
+    떨어뜨리기: 맵돌면서 아래로 다 당기기...
+    반복
+전체 갯수
+*/
