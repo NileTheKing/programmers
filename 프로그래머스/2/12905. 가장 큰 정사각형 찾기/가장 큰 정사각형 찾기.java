603bbd042052c4bb7b23b82bcca7b941b58f5666
HeadedNowhere <nileyangdev@gmail.com>
2026-07-01 13:35:39 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 12.63 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
@@ -1,24 +1,30 @@
 import java.util.*;
-class Solution {
-    public int solution(int[][] board) {
+class Solution
+{
+    public int solution(int [][]board)
+    {
+        int max = 0;
         int m = board.length;
         int n = board[0].length;
         int[][] dp = new int[m][n];
-        int max = 0;
+        //dp초기화
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
+                if (board[i][j] == 0) continue;
                 dp[i][j] = board[i][j];
-                max = Math.max(max, dp[i][j]);
+                max = board[i][j];
             }
         }
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
+                //1채워져있어야 일단 시작. 만약 1이아니면? 거기선 걍 0임
                 if (board[i][j] == 0) continue;
-                dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
-                max = Math.max(max, dp[i][j] * dp[i][j]);
+                int min = Math.min(dp[i][j-1], dp[i-1][j]);
+                min = Math.min(min, dp[i-1][j-1]);
+                dp[i][j] = min + 1;
+                max = Math.max(dp[i][j], max);
             }
         }
-        // for (int[] d : dp) System.out.println(Arrays.toString(d));
-        return max;
+        return max * max;
     }
 }
