4676b5faf62552311d4eb70f1b560b2845d4d4c8
HeadedNowhere <nileyangdev@gmail.com>
2026-07-03 17:44:03 +0900

    [level 3] Title: 2차원 동전 뒤집기, Time: 10.14 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.java
+++ 프로그래머스/3/131703. 2차원 동전 뒤집기/2차원 동전 뒤집기.java
@@ -1,50 +1,34 @@
 class Solution {
-    int min = Integer.MAX_VALUE;
     public int solution(int[][] beginning, int[][] target) {
-        int m = beginning.length; //행(세로)
-        int n = beginning[0].length;//가로
-        for (int i = 0; i < (1 << m); i++) {//0부터 1023까지. 1<<10은 1024니까..우리는 11111 11111까지. 
-            Outter : for (int j = 0; j < (1 << n); j++) {
-                //이제 행뒤집은 스위치, 열 뒤집은 스위치 상태있음.
-                //beginning 기준 스위치 상태보고 뒤집어가지고 target이랑 비교. 다르면 중지
-                //여기서.. 스위치가 01 10이면 뒤집는거고 00 11이면 안뒤집음..이거는 xor일듯
-                //왜냐면 xor은..or인데 exclusive해야하니까.
-                for (int k = 0; k < m; k++) {
-                    for (int l = 0; l < n; l++) {
-                        int before = beginning[k][l];
-                        //현재 k(행) 뒤집힌 여부랑 l뒤집현여부알아야한다.
-                        //소스는 i와 j인데 어케아냐? i의 k번째 비트와 j의l번쨰비트
-                        int rowBit = (i >> k) & 1;
-                        int colBit = (j >> l) & 1;
-                        int after = before ^ rowBit ^ colBit;
-                        
-                        if (after != target[k][l]) continue Outter;
+        //i 모든경우 j모든경우
+            //되는지확인
+            //되면 몇개돌렸는지
+        int m = beginning.length;
+        int n = beginning[0].length;
+        int ans = m + n;
+        for (int i = 0; i < (1 << m); i++) {
+            Outter: for (int j = 0; j < (1 << n); j++) {
+                //이상태로 봤을때 모든칸이 target과 같아야함
+                //테스트!
+                for (int r = 0; r < m; r++) {
+                    for (int c = 0; c < n; c++) {
+                        int afterFlips = beginning[r][c] ^ ((i >> r) & 1) ^ ((j >> c) & 1);
+                        if (afterFlips != target[r][c]) continue Outter;
                     }
                 }
-                //같으면 카운트 업데이트..카운트는? 스위치(i,j) 1갯수 세면 된다.
-                int cnt1 = 0, cnt2 = 0;
-                //i와 j의 비트갯수를 센다. 1갯수.
-                // System.out.printf("========\n");
-                for (int x = 0; x < m; x++) {
-                    if (((i >> x) & 1) == 1) 
-                    {    
-                        cnt1++;
-                        // System.out.printf("행 %d 뒤집음\n", x + 1);
-                    }
+                // System.out.printf("%d %d success\n", i, j);
+                int cnt1 = 0;
+                //모두 같음... 이제 몇개뒤집었는지 카운트
+                for (int k = 0; k < m; k++) {//그니ㅏㄲ i의 각 스위치켜짐을 확인
+                    if (((i >> k) & 1) == 1) cnt1++;
                 }
-                
-                for (int x = 0; x < n; x++) {
-                    if (((j >> x) & 1) == 1) 
-                    {    
-                        cnt2++;
-                        // System.out.printf("행 %d 뒤집음\n", x + 1);
-                    }
+                int cnt2 = 0;
+                for (int k = 0; k < n; k++) {//그니ㅏㄲ i의 각 스위치켜짐을 확인
+                    if (((j >> k) & 1) == 1) cnt2++;
                 }
-                // System.out.printf("i j cnt1 cnt2: %d %d %d %d\n", i,j,cnt1, cnt2);
-                min = Math.min(min, cnt1 + cnt2);
+                ans = Math.min(cnt1+cnt2, ans);
             }
-            
         }
-        return min == Integer.MAX_VALUE ? -1 : min;
+        return ans == m + n ? -1 : ans;
     }
 }
