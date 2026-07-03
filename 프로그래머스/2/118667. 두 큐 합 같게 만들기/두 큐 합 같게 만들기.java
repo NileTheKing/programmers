da46adae67bb3f1adf27fc6689417faada0a1c7b
HeadedNowhere <nileyangdev@gmail.com>
2026-07-03 13:01:28 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 114.97 ms, Memory: 157 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,35 +1,39 @@
 import java.util.*;
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
+
         Queue<Integer> q1 = new LinkedList<>();
         Queue<Integer> q2 = new LinkedList<>();
-        long sum1 = 0, sum2 = 0;
+        long sum1 = 0;
+        long sum2 = 0;
         for (int q : queue1) {
-            q1.add(q);
+            q1.offer(q);
             sum1 += q;
         }
         for (int q : queue2) {
-            q2.add(q);
+            q2.offer(q);
             sum2 += q;
         }
+        // System.out.printf("sum1 sum2 %d %d\n", sum1, sum2);
         int cnt = 0;
-        int limit = queue1.length * 3 + 1;
-        while (sum1 != sum2 && cnt < limit) {
-            // if (q1.isEmpty() && q2.isEmpty()) break;
-            if (sum1 > sum2) {
+        int limit = queue1.length * 3;
+        while (cnt <= limit) {
+            if (sum1 == sum2) {
+                return cnt;
+            }else if(sum1 > sum2) {
                 int polled = q1.poll();
-                q2.offer(polled);
-                sum2 += polled;
                 sum1 -= polled;
+                sum2 += polled;
+                q2.offer(polled);
             }else {
                 int polled = q2.poll();
-                q1.offer(polled);
-                sum1 += polled;
                 sum2 -= polled;
+                sum1 += polled;
+                q1.offer(polled);
             }
             cnt++;
+            
         }
-        // System.out.printf("%d %d and cnt %d\n", sum1, sum2, cnt);
         return cnt >= limit ? -1 : cnt;
     }
 }
