7e14969555b96a583dc227852d17ddc198461d01
HeadedNowhere <nileyangdev@gmail.com>
2026-07-03 12:33:35 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 29.54 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.java
@@ -1,30 +1,30 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] gems) {
-        Set<String> target = new HashSet<>();
-        for (String g : gems) target.add(g);
+        Set<String> set = new HashSet<>();
+        for (String g : gems) set.add(g);
         
-        //substring. 근데 현재상태 보관할거 필요. hashmap으로 관리하다가 set이랑key비교
-        Map<String, Integer> current = new HashMap<>();
-        int l = 0;
+        Map<String, Integer> map = new HashMap<>();
         int[] ans = new int[2];
-        int minLength = Integer.MAX_VALUE;
-        for (int r = 0; r < gems.length; r++) {
-            //일단 확장하고 줄일수있을떄까지 축소
-            current.put(gems[r], current.getOrDefault(gems[r], 0) + 1);
-            //축소 및 갱신
-            while (current.keySet().size() == target.size()) {
-                //지금 current의 keyset이랑 target이랑 동일한지확인
-                //되면 갱신하고 줄임.
-                //비교해야햐ㅏ는데 딸깍 가능?그냥해라
-                if (r - l + 1 < minLength) {//갱신
+        int min = gems.length + 1;
+        int l = 0;
+        for (int i = 0; i < gems.length; i++) {
+            // System.out.printf("===i : %d====\n", i);
+            map.put(gems[i], map.getOrDefault(gems[i], 0) + 1);//일단 늘림
+            //만약 있으면 될떄까지 축소. 될떄마다 갱신
+            while (l <= i && set.size() == map.keySet().size()) {
+                // System.out.printf("l = %d\n", l);
+                if (i - l + 1 < min) {
+                    // System.out.printf("updated\n");
                     ans[0] = l + 1;
-                    ans[1] = r + 1;
-                    minLength = r - l + 1;
+                    ans[1] = i + 1;
+                    min = i - l + 1;
                 }
-                //l 줄이기
-                current.put(gems[l], current.get(gems[l])- 1);//줄이기
-                if (current.get(gems[l]) == 0) current.remove(gems[l]);//감소
+                //l감소하고 0이면 아예삭제
+                map.put(gems[l], map.getOrDefault(gems[l], 0) -1);
+                if (map.get(gems[l]) <= 0) {
+                    map.remove(gems[l]);
+                }
                 l++;
             }
         }
