e9082b4518b3c39a8378eddc92d7e447db89b452
HeadedNowhere <nileyangdev@gmail.com>
2026-07-04 18:42:23 +0900

    [level 2] Title: 디펜스 게임, Time: 232.54 ms, Memory: 221 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -5,18 +5,26 @@ class Solution {
         int r = enemy.length;
         int ans = 0;
         while (l <= r) {
-            int m = l + (r - l) / 2;
-            //top k
-            //전체sum
-            int[] partialEnemy = Arrays.copyOfRange(enemy, 0, m); //이러면 m-1인덱스까지..그럼m라운드까지 잘림
-            Arrays.sort(partialEnemy);
-            long sum = 0;
-            for (int i = 0; i < partialEnemy.length - k; i++) {
-                sum += partialEnemy[i];
+            int m = (l + r) / 2;
+            //m라운드까지 갔다치자. 그러면 m라운드 치르고.. sum
+            //그중에 top 3 빼면 성공
+            //성공이란
+            //예를 들어  4 2 4 5 3 3 1에서
+            //답 5라운드까지가면 18인데 k가 3이라 13뺴주면 5임..그러면2가남아서 ㅇㅋ
+            //m까지 자르고 정렬하고 sum구하고 top3
+            // System.out.printf("===== m = %d =====\n", m);
+            int[] cut = Arrays.copyOf(enemy, m);
+            Arrays.sort(cut);
+            long sum = 0;//m = 5... length 5.. k 3이면 0 1 2 3 4에서 2까지만
+            //length 5 k 3이면 0 1 2까지해야함 그니까
+            for (int i = 0; i < cut.length - k; i++) {
+                // System.out.printf("%d, adding %d\n", i, cut[i]);
+                sum += cut[i];
             }
-            // System.out.printf("m: %d, partialSum: %d, topk: %d, sum: %d\n", m, partialSum, topKSum, sum);
-            if (n >= sum) {
-                ans = Math.max(m, ans);
+            // System.out.printf("sum = %d\n", sum);
+            //sum이 n이하이면 m라운드가능.
+            if (sum <= n) {
+                ans = m;
                 l = m + 1;
             }else {
                 r = m - 1;
@@ -25,9 +33,3 @@ class Solution {
         return ans;
     }
 }
-/**
-무적권쓰냐 안쓰냐 백트래킹하면 터짐
-라운드 10^7 라서 ON아니면 터짐. 그러면 enemy순회가아니다
-이분탐색을 생각해보자.
-막을수 있냐 없냐여부는. 라운드까지갔다치자. 되려면? 그라운드까지 enemy배열잘라서? 무적권은 무조건 쓰는게 이득이기떄문에..그중에 top k개 자르고.. 나머지애들의 합이 n보다 작으면 된다.
-*/
