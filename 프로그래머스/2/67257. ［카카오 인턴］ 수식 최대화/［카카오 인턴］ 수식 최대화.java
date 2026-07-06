8ab3f2c67d6ab6797a5b3c78745739e3e07103cc
HeadedNowhere <nileyangdev@gmail.com>
2026-07-06 16:43:01 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 1.49 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -2,74 +2,93 @@ import java.util.*;
 class Solution {
     public long solution(String expression) {
         long max = 0;
-        //6가지 경우의수 순회
         char[][] candidates = getCandidates();
-        //list로 분리해야하나? 만들어야한다. 근데 어떻게?
-        List<String> list = getList(expression);
-
-        for (char[] c : candidates) {
-            List<String> copy = new ArrayList<>(list);
-            // System.out.printf("====try====\n");
-            for(char operator : c) {
-                //이제 copy를 순회해야함근데 길이가 바뀜..
-                int size = copy.size();
-                for (int i = 0; i < size; i++) {
-                    if (copy.get(i).charAt(0) == operator) {
-                        long val1 = Long.parseLong(copy.get(i - 1));
-                        long val2 = Long.parseLong(copy.get(i + 1));
-                        long calc = calc(val1, val2, operator);
-                        copy.remove(i - 1);
-                        copy.remove(i - 1);
-                        copy.remove(i - 1);
-                        copy.add(i - 1, String.valueOf(calc));
-                        i = i - 1;
-                        size -= 2;
-                    }
+        
+        List<String> parsed = getParsedList(expression);
+        for (char[] candi : candidates) { //우선순위시도
+            //parse복사해서 candi순회하며 해당 연산자 계산.
+            List<String> copied = new ArrayList<>(parsed);
+            for (char operator : candi) {
+                for (int i = 0; i < copied.size(); i++) {
+                    if (copied.get(i).length() != 1) continue;
+                    if (copied.get(i).charAt(0) != operator) continue;
+                     //연산자
+                    long n1 = Long.parseLong(copied.get(i - 1));
+                    long n2 = Long.parseLong(copied.get(i + 1));
+                    long number = getNumber(n1, n2, operator);
+                    //100 - 200 * 300 - 500 + 20에서
+                    //100 - 200이라치자.
+                    copied.remove(i - 1);
+                    copied.remove(i - 1);
+                    copied.remove(i - 1); //다지움
+                    copied.add(i - 1, String.valueOf(number));
+                    //원래 i = 1이었음. 이제는 i그디로 봐야하는거아님?
+                    i--;
                 }
-                // System.out.println(copy);
             }
-            max = Math.max(max, Math.abs(Long.parseLong(copy.get(0))));
+            //모든 연산자 찾음.
+            // for (String str : copied) System.out.println(str);
+            // System.out.printf("=====\n");
+            long val = calculate(copied);
+            max = Math.max(val, max);
         }
         return max;
     }
-    public long calc(long n1, long n2, char operator) {
+    public long getNumber(long n1, long n2, char operator) {
         if (operator == '+') {
             return n1 + n2;
-        }else if(operator == '*') {
-            return n1 * n2;
-        }else {
+        }else if (operator == '-') {
             return n1 - n2;
+        }else {
+            return n1 * n2;
         }
     }
-    public List<String> getList(String str) {
-        int ptr = 0;
+    
+    public long calculate(List<String> list) {
+        if (list.size() == 1) return Math.abs(Long.parseLong(list.get(0)));
+        // if (list.size() != 3) System.out.printf("NOT 3\n");
+        if (list.get(1).charAt(0) == '+') {
+            return Math.abs(Long.parseLong(list.get(0)) + Long.parseLong(list.get(2)));
+        }else if(list.get(1).charAt(0) == '-') {
+            return Math.abs(Long.parseLong(list.get(0)) - Long.parseLong(list.get(2)));
+        }else {
+            return Math.abs(Long.parseLong(list.get(0)) * Long.parseLong(list.get(2)));
+        }
+        
+    }
+    public List<String> getParsedList(String expression) {
         List<String> res = new ArrayList<>();
-        while (ptr < str.length()) {
-            int tmp = ptr;
-            while (tmp < str.length() && Character.isDigit(str.charAt(tmp))) {
+        int ptr = 0;
+        while (ptr < expression.length()) {
+            int tmp = ptr + 1;
+            while (tmp < expression.length() && Character.isDigit(expression.charAt(tmp))) {
                 tmp++;
             }
-            res.add(str.substring(ptr, tmp));
-            if (tmp < str.length())
-                res.add(String.valueOf(str.charAt(tmp)));
+            //이제 연산자임
+            res.add(expression.substring(ptr, tmp));
+            if (tmp < expression.length()) res.add(expression.substring(tmp, tmp + 1));
             ptr = tmp + 1;
         }
         return res;
     }
     public char[][] getCandidates() {
-        return new char[][] {
+        return new char[][]{
             {'+','-','*'},
             {'+','*','-'},
-            {'-','+','*'},
-            {'-','*','+'},
             {'*','-','+'},
-            {'*','+','-'}
+            {'*','+','-'},
+            {'-','+','*'},
+            {'-','*','+'}
         };
     }
 }
 /**
-6개 배열(경우의수 3!)
-expression순회하면서 6가지 경우의수를 다 한다. 각 경우에 대해 최댓값갱신
-각 경우는 우선순위에 맞는 수식찾으면 계산을 한다.
-시간복잡도 O(n * 6 * 3 * n) ->가능
+연산자3개존재. 우선순위 경우의수는 6개가능. bruteforce가능. 길이 100미만이니까..연산자가
+operand + operator = 100
+operator가 99가능? operand가1이니까 안되지
+길이최대 대충50이려나.그러면 금방하지.. 100 * 6수준?
+
+글우선수위정해두고
+파싱해가지고 우선순위에맞는거 처리 -> 계산
+비교
 */
