bd46e8a3cc7519bd42632c709c96062da0047d66
HeadedNowhere <nileyangdev@gmail.com>
2026-07-07 15:58:31 +0900

    [level 3] Title: 여행경로, Time: 1.14 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -2,39 +2,36 @@ import java.util.*;
 class Solution {
     Map<String, PriorityQueue<String>> map;
     List<String> ans;
+    int targetLength;
     public String[] solution(String[][] tickets) {
         map = new HashMap<>();
-        
         ans = new ArrayList<>();
+        targetLength = tickets.length;
         for (String[] t : tickets) {
-            map.computeIfAbsent(t[0], k -> new PriorityQueue<>())
-                .offer(t[1]);
-            //leftOver.add(t[0] + t[1]);
+            map.computeIfAbsent(t[0], k -> new PriorityQueue<>()).offer(t[1]);
         }
-        backtrack("ICN", new ArrayList<>(), tickets.length);
-        
+        List<String> path = new ArrayList<>();
+        backtrack("ICN", path);
         return ans.toArray(new String[0]);
     }
-    public boolean backtrack(String current, List<String> path, int length) {
+    public boolean backtrack(String current, List<String> path) {
         path.add(current);
-        if (path.size() == length + 1) { //티켓 다씀!성공.
+        if (path.size() == targetLength + 1) {
             ans = new ArrayList<>(path);
             return true;
         }
-        //path.add(current);
+        
         PriorityQueue<String> original = map.get(current);
-        if (original == null || original.isEmpty()) {
+        if (original == null) {
             path.remove(path.size() - 1);
             return false;
         }
-        PriorityQueue<String> pq = new PriorityQueue<>(original);
-        while (pq != null && !pq.isEmpty()) {
-            String polled = pq.poll();
+        PriorityQueue<String> copiedPq = new PriorityQueue<>(original);
+        while (!copiedPq.isEmpty()) {
+            String polled = copiedPq.poll();
             map.get(current).remove(polled);
-            if (backtrack(polled, path, length)) return true;
+            if (backtrack(polled, path)) return true;
             map.get(current).offer(polled);
-            //여기서 pq또 넣나..?또 넣으면 무한반복이고.. 안넣으면 복구가안되는ㄷ.
-            //아니 복구할필요가 없지 안되는거니까. 걔가 안되면 뒤에서 찾아줄겨.
         }
         path.remove(path.size() - 1);
         return false;
