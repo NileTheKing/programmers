86ced5109421a5d329c547f610c7f35a67381fb6
HeadedNowhere <nileyangdev@gmail.com>
2026-07-18 19:29:32 +0900

    [level 3] Title: 선입 선출 스케줄링, Time: 2.37 ms, Memory: 57.9 MB -BaekjoonHub

--- 프로그래머스/3/12920. 선입 선출 스케줄링/README.md
+++ 프로그래머스/3/12920. 선입 선출 스케줄링/README.md
@@ -1,10 +1,10 @@
 # [level 3] 선입 선출 스케줄링 - 12920 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12920) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12920#) 
 
 ### 성능 요약
 
-메모리: 55.7 MB, 시간: 6.11 ms
+메모리: 57.9 MB, 시간: 2.37 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 03일 17:09:46
+2026년 07월 18일 19:29:28
 
 ### 문제 설명
 
--- 프로그래머스/3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.java
+++ 프로그래머스/3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.java
@@ -1,38 +1,30 @@
-
 class Solution {
     public int solution(int n, int[] cores) {
-        if (cores.length >= n) return n;
+        if (n <= cores.length) return n;
         int l = 0;
-        int r = 50000 * 10000 / 2;
-        int t = r; //걸리는 시간 계산하는거임.
+        int r = 10000 * 50000 / 2;
+        int t = r;
+        //배정 다하는 T를 구함
         while (l <= r) {
-            int m = l + (r - l) / 2;
-            int cnt = cores.length;
-            //m시간 안에 다 할 수 있는지 확인.지금은 정확히 찾는게 아니라 되는지만..
-            //되는지 어떻게보냐면.. m으로 가능한지인데
-            for (int c : cores) {
-                cnt += m / c; //m이라는 시간동안 c코어가 c걸리는데 몇개했는지
-                if (cnt >= n) break;
-            }
-            //System.out.printf("for trying time %d, did %d\n", m, cnt);
-            if (cnt < n) { //시간이 너무적었다
-                l = m + 1;
-            }else if (cnt >= n) {
-                r = m - 1;
+            int m = (l + r) / 2;
+            long cnt = cores.length;
+            for (int c : cores) cnt += (m / c);
+            if (cnt >= n) {//배정가능
                 t = m;
+                r = m - 1;
+            }else {
+                l = m + 1;
             }
         }
-        // System.out.printf("T is %d\n", t);
-        //t구했음.
-        //이제 t-1까지 몇개끝냈는지
-        int doneTil = cores.length;
-        for (int c : cores) {
-            doneTil += (t - 1) / c;
-        }
-        int left = n - doneTil; //남은갯수
-        // System.out.printf("need to do %d\n",left);
+        //배정 다못하는 T - 1에서는 다 배정을안했을거고 T에는 모든 배정이완료
+        //즉 T-1까지 일하고 몇개남는지 안다음에
+        //T에서 직접시뮬레이션
+        int t_minus = t - 1;
+        int done_t_minus = cores.length;
+        for (int c : cores) done_t_minus += (t_minus / c);
+        int left = n - done_t_minus;
         for (int i = 0; i < cores.length; i++) {
-            //이시간에 작업중인애들빼고 작업없는 애들
+            //c = 2, t = 5... 일중.. 4면일안함
             if (t % cores[i] != 0) continue;
             left--;
             if (left == 0) return i + 1;
@@ -40,3 +32,7 @@ class Solution {
         return -1;
     }
 }
+/**
+    1  2  3
+0:  1  2  3
+*/
