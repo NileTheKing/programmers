b189da1c4af1c4832cee945da4e4fdbbf4f92697
HeadedNowhere <nileyangdev@gmail.com>
2026-07-22 16:29:41 +0900

    [level 4] Title: 징검다리, Time: 22.54 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -1,37 +1,38 @@
 import java.util.*;
 class Solution {
     public int solution(int distance, int[] rocks, int n) {
+        Arrays.sort(rocks);
         int l = 0;
         int r = distance;
-        int ans = 0;
-        Arrays.sort(rocks);
-        
+        int ans = distance;
         while (l <= r) {
-            int m = l + (r - l) / 2;
-            //바위간 거리가 m을 넘을 수 없다
-            
-            //실제로 검증... 2개지울수있는데 
-            int cnt = 0;
-            int start = 0;
-            int prev = 0;;
-            for(int rock : rocks) { //바위 사이 거리가m보다 작으면 안돼. 지워야해
-                if (rock - prev < m) {//m보다 작을수없어 m은최소거리임
-                    //rock은없어져야함
+            // System.out.printf("=======\n");
+            int m = l + (r - l) / 2; //최소거리가 이거야야한다
+            // System.out.printf("m = %d\n",m);
+            int cnt = 0;//제거한갯수..너무 멀면 밟아야하고 아니면 제거가능
+            int prev = 0;
+            for (int ro : rocks) {
+                // System.out.printf("checking %d  ..", ro);
+                if (ro - prev < m) {//너무 가까워.. 제거해야해
                     cnt++;
-                }else { //얜 그대로있어야해
-                    prev = rock;
+                    // System.out.printf("%d 제거\n", ro);
+                }else {//얜 제거안해도됨
+                    // System.out.println();
+                    prev = ro;
                 }
             }
             if (distance - prev < m) cnt++;
-            //System.out.printf("m = %d, cnt = %d\n", m, cnt);
-            if (cnt > n) { // 바위사이 거리중에 제일 작은게 m이려면 너무 많이 지워야함-> m 은 욕심이야
+            // System.out.printf("cnt=%d\n", cnt);
+            //제거하고나니까 min이 너무커서.. 제거를 많이해야한다 -> min지키기엔 n이빡세다
+            if (cnt > n) {
+                //너무 빡셌으니까 m줄여야
                 r = m - 1;
-            }else { //거리최소가 m으로 하려했는데 너무 널널해서 뭐 안지워도됨.
-                l = m + 1;
+            }else {//가능..업데이트
+                // System.out.printf("updating: %d\n", m);
                 ans = m;
+                l = m + 1;//더 도전
             }
         }
-        
         return ans;
     }
 }
