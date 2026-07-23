ed4482d30cdf04ba9fc8f065083324ae5a910963
HeadedNowhere <nileyangdev@gmail.com>
2026-07-23 19:46:55 +0900

    [level 3] Title: 정수 삼각형, Time: 10.92 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/README.md
+++ 프로그래머스/3/43105. 정수 삼각형/README.md
@@ -1,10 +1,10 @@
 # [level 3] 정수 삼각형 - 43105 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43105?gad_source=1&gad_campaignid=23716289893&gbraid=0AAAAAC_c4nB9xkiVZFsqw-uD3NZz3N_7g&gclid=Cj0KCQjw94bTBhDQARIsAN3vv0zRvshnKp0Qn5m0csEDG8hMKpkm_Xbs_N-UD1q2oR1MjpQBMwYmb2YaAjDoEALw_wcB) 
 
 ### 성능 요약
 
-메모리: 64.9 MB, 시간: 12.69 ms
+메모리: 64 MB, 시간: 10.92 ms
 
 ### 구분
 
@@ -16,11 +16,11 @@
 
 ### 제출 일자
 
-2025년 03월 10일 19:58:38
+2026년 07월 23일 19:46:53
 
 ### 문제 설명
 
-<p><img src="https://grepp-programmers.s3.amazonaws.com/files/production/97ec02cc39/296a0863-a418-431d-9e8c-e57f7a9722ac.png" title="" alt="스크린샷 2018-09-14 오후 5.44.19.png"></p>
+<p><img src="https://asset.programmers.co.kr/files/production/97ec02cc39/296a0863-a418-431d-9e8c-e57f7a9722ac.png" title="" alt="스크린샷 2018-09-14 오후 5.44.19.png"></p>
 
 <p>위와 같은 삼각형의 꼭대기에서 바닥까지 이어지는 경로 중, 거쳐간 숫자의 합이 가장 큰 경우를 찾아보려고 합니다. 아래 칸으로 이동할 때는 대각선 방향으로 한 칸 오른쪽 또는 왼쪽으로만 이동 가능합니다. 예를 들어 3에서는 그 아래칸의 8 또는 1로만 이동이 가능합니다.</p>
 
--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,40 +1,24 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] triangle) {
+        int[][] dp = new int[triangle.length][triangle[triangle.length - 1].length];
         
-        
-        
-      
-        //1열에 해당하는 부분부터 완성
-        for (int i = 1; i < triangle.length; i++) {
-            for (int j = 0; j < triangle[i].length;j++) {
-                //첫번째면 경계쳐리
-                if (j == 0) {
-                    triangle[i][j] = triangle[i-1][j] + triangle[i][j];
-                }//마지막이면 또 경계처리
-                else if(j == triangle[i].length - 1){
-                    triangle[i][j] = triangle[i - 1][j - 1] + triangle[i][j];
-                }
-                else {
-                    triangle[i][j] = Math.max(triangle[i-1][j-1], triangle[i-1][j]) + triangle[i][j];
-                }
+        for (int[] d: dp) Arrays.fill(d, Integer.MIN_VALUE);
+        dp[0][0] = triangle[0][0];
+        int m = dp.length;
+        int n = dp[0].length;
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j <= i; j++) {
+                int val = triangle[i][j];
+                int lookLeft = j == 0 ? 0 : j - 1;
+                int lookRight = j == n - 1 ? n - 1 : j;
+                //triangle[i][j]는 안전..핳ㅁ
+                dp[i][j] = Math.max(dp[i - 1][lookLeft] + val, dp[i - 1][lookRight] + val);
             }
         }
-        
-
-        
-        return Arrays.stream(triangle[triangle.length - 1]).max().getAsInt();
+        for (int i = 0; i < m; i++) {
+            // System.out.println(Arrays.toString(dp[i]));
+        }
+        return Arrays.stream(dp[m - 1]).max().getAsInt();
     }
 }
-/**
-트리?
-음 백트래킹으로 할 수 있을 것 같은데..
-그것도 그거고 dp이용해가지고 할 수 있을듯? 왜냐하면 유지되기 때문이라
-그렇다면 특정 위치에서 최댓값으로 이동할 수 있는 방법은 단 하나일테니까 그거를 저장해가지고 탐색할 수 있을듯?
-
-triangle이거 만든 다음에 그  뭐냐 가로세로 경우의수 하는 것 처럼 비교해서 더 큰 방법으로 계속해가지고 
-완성된 배열의 마지막 행 순회해서 최댓값 추적해도 될듯?\\
-
-dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j])
-
-*/
