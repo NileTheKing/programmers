35547af4f15833afed3c78ce1f6996218d60f850
HeadedNowhere <nileyangdev@gmail.com>
2026-07-23 19:07:31 +0900

    [level 3] Title: N으로 표현, Time: 13.42 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,65 +1,36 @@
 import java.util.*;
 class Solution {
     public int solution(int N, int number) {
+        if (N == number) return 1;
+        Set<Integer>[] dp = new Set[10];
+        dp[0] = new HashSet<>();
+        dp[1] = new HashSet<>();
+        dp[1].add(N);
         
-        List<Set<Integer>> dp = new ArrayList<>();
-        
-        //초기화
-        for (int i = 0; i < 9; i++) {//9개 만들면 마지막 인덱스가 8
-            dp.add(new HashSet<>());
-        }
-        for (int i = 1; i <= 8; i++) {
-            int repeated = Integer.parseInt(String.valueOf(N).repeat(i));
-            dp.get(i).add(repeated);
-        }
-        //순회하며 모든 조합만들기
-        for (int i = 2; i <= 8; i++) { //i는 N갯수
-            Set<Integer> currentSet = dp.get(i);
-            for (int j = 1; j < i; j++) {
-                //i개인 수로 만들 수 있는 조합은. 예를 들어 4라치자.
-                //또 i-j + j 으로 나타낼 수 있음. 예를 들면 j가 1이면 i -j인 3이이서
-                // 1+3, 2+2로 할 수 있음
-                Set<Integer> a = dp.get(j);
-                Set<Integer> b = dp.get(i - j);
-                for (int aa : a) {
-                    for (int bb : b) {
-                        currentSet.add(aa + bb);
-                        currentSet.add(aa - bb);
-                        currentSet.add(aa * bb);
-                        if (bb != 0) currentSet.add(aa / bb);
+        for (int i = 2; i <= 8; i++) {
+            dp[i] = new HashSet<>();
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < i; j++) sb.append(N);
+            dp[i].add(Integer.parseInt(sb.toString()));
+            
+            for (int j = 1; j < i; j++) {//앞피연산자의숫자갯수
+                int k = i - j; //뒷 피연산자의 수
+                for (int a : dp[j]) {
+                    for (int b : dp[k]) {
+                        dp[i].add(a + b);
+                        dp[i].add(a - b);
+                        dp[i].add(a * b);
+                        if (b != 0) dp[i].add(a / b);
                     }
                 }
+                
             }
+            if (dp[i].contains(number)) return i;
         }
-        for (int i = 1; i <= 8; i++) {
-            if (dp.get(i).contains(number)) return i;
-        }
-        
         return -1;
-        
-        
     }
-}
-/**
-dp를 이용해서 풀기
-그러면은 bottom up/ top-down
-if number = 12 N=5
-1 2 3 4 5 6 7 8 9 10 11 12
-bottom-up 
-12를 만드느는 조합. 1개로 2개로 3개로 ..... 
-1개는 A
-2개 A+B
-3개 A+B+C
-4개 A+B+C+D
-결국 A+B형식으로 표현 가능
-
-5 1개로 만들 수 있는 숫자 만들고
-5 2개로 만들 수 있는 숫자 만들고.
-5 3개로 만들 수 있는 숫자 만들고 (위에서 만든 조합과 5를 사칙연산 한 결과를 추가)
-5 4개 (5 3개로 만들수 있는 숫자와 5 사칙연산한 겨우우ㅢ 수 추가)
-...여기서 12나오면 스탑.
-
-
-
+}/**
+재활용가능.. 지금 문제작아지고 활용하는거를 떠올렸는데 구현을 못하는게병목
+기존 dp는 점화식으로 하는거고 이거는 음.. 뭔가 흠....
 
 */
