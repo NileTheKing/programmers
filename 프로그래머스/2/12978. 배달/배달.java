1be37ed20d307958161246621c8722b7bfbaec72
HeadedNowhere <nileyangdev@gmail.com>
2026-07-24 15:00:33 +0900

    [level 2] Title: 배달, Time: 4.95 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,47 +1,38 @@
 import java.util.*;
 class Solution {
     public int solution(int N, int[][] road, int K) {
+        //graph완성
         List<List<int[]>> graph = new ArrayList<>();
         for (int i = 0; i <= N; i++) graph.add(new ArrayList<>());
         for (int[] r : road) {
             int v1 = r[0];
             int v2 = r[1];
-            int weight = r[2];
-            graph.get(v1).add(new int[] {v2, weight});
-            graph.get(v2).add(new int[] {v1, weight});
+            int cost = r[2];
+            graph.get(v1).add(new int[] {v2, cost});
+            graph.get(v2).add(new int[] {v1, cost});
         }
-        int[] dist = new int[N + 1];
+        //dijkstra
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
+        pq.offer(new int[] {1, 0});
+        int[] dist = new int[N+1];
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[1] = 0;
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(1); //초기
-        while (!q.isEmpty()) {
-            int polled = q.poll();
-            List<int[]> neis = graph.get(polled);
-            for (int[] nei : neis) {
-                int v = nei[0];
-                int w = nei[1];
-                if (dist[v] < dist[polled] + w) continue;//이미 더 좋은경로가있따
-                q.offer(v);
-                dist[v] = dist[polled] + w;
+        while (!pq.isEmpty()) {
+            int[] polled = pq.poll();
+            if (dist[polled[0]] < polled[1]) continue;
+            //가지치기..visit은 dist로하는거니까 이미한거지. 위에서. /아래놈은 호출전에할거
+            for (int[] nei : graph.get(polled[0])) {
+                if (dist[nei[0]] <= polled[1] + nei[1]) continue;
+                pq.offer(new int[] {nei[0], polled[1] + nei[1]});
+                dist[nei[0]] = polled[1] + nei[1];
             }
         }
+        //dist순회
         int cnt = 0;
         for (int i = 1; i <= N; i++) {
-            if (dist[i] <= K) {
-                // System.out.printf("house: %d, time: %d\n", i, dist[i]);
-                cnt++;
-            }
+            if (dist[i] <= K) cnt++;
         }
+
         return cnt;
     }
 }
-/**
-최단거리를 업데이트해야한다 .. 기준점하나 -> 다익스트라
-다익스트라 어케하냐? 까먹음
-하지만 논리적으로 생각해보면
-1. 출발해서 q에 넣는다
-2. 꺼내가지고 ,.. 출발점기준 현재거리까지 한 기록한게있는데..이거보다 작으면 업데이트하고 또 넣어
-
-dist[]배열이있고
-*/
