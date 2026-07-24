4d281f9c466ddedfe68961b338109981f18f7808
HeadedNowhere <nileyangdev@gmail.com>
2026-07-24 17:39:04 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 1.44 ms, Memory: 94.9 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -1,94 +1,69 @@
 import java.util.*;
 class Solution {
     public long solution(String expression) {
-        long max = 0;
-        char[][] candidates = getCandidates();
+        char[][] priorities = getPriorities();
+        List<String> operands = new ArrayList<>();
+        List<String> operators = new ArrayList<>();
+        parse(operands, operators, expression);
         
-        List<String> parsed = getParsedList(expression);
-        for (char[] candi : candidates) { //우선순위시도
-            //parse복사해서 candi순회하며 해당 연산자 계산.
-            List<String> copied = new ArrayList<>(parsed);
-            for (char operator : candi) {
-                for (int i = 0; i < copied.size(); i++) {
-                    if (copied.get(i).length() != 1) continue;
-                    if (copied.get(i).charAt(0) != operator) continue;
-                     //연산자
-                    long n1 = Long.parseLong(copied.get(i - 1));
-                    long n2 = Long.parseLong(copied.get(i + 1));
-                    long number = getNumber(n1, n2, operator);
-                    //100 - 200 * 300 - 500 + 20에서
-                    //100 - 200이라치자.
-                    copied.remove(i - 1);
-                    copied.remove(i - 1);
-                    copied.remove(i - 1); //다지움
-                    copied.add(i - 1, String.valueOf(number));
-                    //원래 i = 1이었음. 이제는 i그디로 봐야하는거아님?
+        //<--->above checked
+        long ans = Integer.MIN_VALUE;
+        for (char[] pri : priorities) {//각우선순위에맞게 테스트
+            List<String> copiedOperands = new ArrayList<>(operands);
+            List<String> copiedOperators = new ArrayList<>(operators);
+            for (char op : pri) { //순서에 맞는애 처리
+                int size = copiedOperators.size();
+                for (int i = 0; i < size; i++) {
+                    if (op != copiedOperators.get(i).charAt(0)) continue;
+                    String num1 = copiedOperands.get(i);
+                    String num2 = copiedOperands.get(i+1);
+                    String calculated = calculate(num1, num2, op);
+                    copiedOperators.remove(i);
+                    copiedOperands.remove(i);
+                    copiedOperands.remove(i);
+                    copiedOperands.add(i, calculated);
                     i--;
+                    size--;
                 }
             }
-            //모든 연산자 찾음.
-            // for (String str : copied) System.out.println(str);
-            // System.out.printf("=====\n");
-            long val = calculate(copied);
-            max = Math.max(val, max);
+            //연산 끝난애 계산
+            long val = Math.abs(Long.parseLong(copiedOperands.get(0)));
+            ans = Math.max(val, ans);
         }
-        return max;
+        return ans;
     }
-    public long getNumber(long n1, long n2, char operator) {
-        if (operator == '+') {
-            return n1 + n2;
-        }else if (operator == '-') {
-            return n1 - n2;
-        }else {
-            return n1 * n2;
-        }
-    }
-    
-    public long calculate(List<String> list) {
-        if (list.size() == 1) return Math.abs(Long.parseLong(list.get(0)));
-        // if (list.size() != 3) System.out.printf("NOT 3\n");
-        if (list.get(1).charAt(0) == '+') {
-            return Math.abs(Long.parseLong(list.get(0)) + Long.parseLong(list.get(2)));
-        }else if(list.get(1).charAt(0) == '-') {
-            return Math.abs(Long.parseLong(list.get(0)) - Long.parseLong(list.get(2)));
-        }else {
-            return Math.abs(Long.parseLong(list.get(0)) * Long.parseLong(list.get(2)));
-        }
-        
-    }
-    public List<String> getParsedList(String expression) {
-        List<String> res = new ArrayList<>();
+    String calculate(String n1, String n2, char op) {
+        if (op == '+') return String.valueOf(Long.parseLong(n1) + Long.parseLong(n2));
+        if (op == '*') return String.valueOf(Long.parseLong(n1) * Long.parseLong(n2));
+        if (op == '-') return String.valueOf(Long.parseLong(n1) - Long.parseLong(n2));
+        return "";
+    } 
+    void parse(List<String> operands, List<String> operators, String expression) {
         int ptr = 0;
-        while (ptr < expression.length()) {
+        int length = expression.length();
+        while (ptr < length) {
             int tmp = ptr + 1;
-            while (tmp < expression.length() && Character.isDigit(expression.charAt(tmp))) {
-                tmp++;
+            //숫자아닌거(연산자)나올떄까지 증가
+            while (tmp < length && 
+                   Character.isDigit(expression.charAt(tmp))){
+                tmp++;   
             }
-            //이제 연산자임
-            res.add(expression.substring(ptr, tmp));
-            if (tmp < expression.length()) res.add(expression.substring(tmp, tmp + 1));
+            //ptr tmp substring
+            operands.add(expression.substring(ptr, tmp));
+            //tmp는 이제 연산자
+            if (tmp < length) operators.add(String.valueOf(expression.charAt(tmp)));
             ptr = tmp + 1;
         }
-        return res;
+        return;
     }
-    public char[][] getCandidates() {
-        return new char[][]{
-            {'+','-','*'},
-            {'+','*','-'},
-            {'*','-','+'},
+    char[][] getPriorities() {
+        return new char[][] {
             {'*','+','-'},
+            {'*','-','+'},
+            {'+','*','-'},
+            {'+','-','*'},
             {'-','+','*'},
-            {'-','*','+'}
+            {'-','*','+'},
         };
     }
 }
-/**
-연산자3개존재. 우선순위 경우의수는 6개가능. bruteforce가능. 길이 100미만이니까..연산자가
-operand + operator = 100
-operator가 99가능? operand가1이니까 안되지
-길이최대 대충50이려나.그러면 금방하지.. 100 * 6수준?
-
-글우선수위정해두고
-파싱해가지고 우선순위에맞는거 처리 -> 계산
-비교
-*/
