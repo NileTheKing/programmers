6c2ced48a62e77062ed12173c5c27bb92413bd22
HeadedNowhere <nileyangdev@gmail.com>
2026-07-24 15:44:11 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.04 ms, Memory: 58 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -1,22 +1,24 @@
 import java.util.*;
 class Solution {
     public int[] solution(int n, long k) {
-        long[] factorial = new long[n+1];
-        List<Integer> nums = new ArrayList<>();
+        long[] factorial = new long[n + 1];
         factorial[0] = 1;
-        for (int i = 1; i <= n; i++) {
-            factorial[i] = factorial[i - 1] * i;
-            nums.add(i);
-        }
+        for (int i = 1; i <= n; i++) factorial[i] = factorial[i - 1] * i;
+        List<Integer> bucket = new ArrayList<>();
+        for (int i = 0; i < n; i++) bucket.add(i + 1);//<<이거도맞는지모름
+        //1 2 3 4 5
+        //0 1 2 3 4
+        // for (int b : bucket) System.out.printf("%d ", b);
         int[] ans = new int[n];
-        k--;
-        for (int i = 0; i < n; i++) {
-            long fact = factorial[n - 1 - i];
-            int idx = (int)(k / fact); //몫->무슨바구니
-            ans[i] = (nums.get(idx));
-            nums.remove(idx);
-            //나머지 -> 바구니에서 위치 -> 지금은 바구니 내 위치지만 다음 전체위치..
-            k %= fact;
+        k--; //0index..몫과나머지 컴퓨터용
+        //몫은 어느바구니 나머지는 바구니 내 위치
+        for (int i = 1; i <= n; i++) {
+            //첫번째자리수
+            long fact = factorial[n - i]; //뒤에나올애들
+            int idx = (int) (k / fact);
+            ans[i-1] = bucket.get(idx);
+            bucket.remove(idx);
+            k = k % fact;//다음 바구니는..현재바구니내위치가
         }
         return ans;
     }
