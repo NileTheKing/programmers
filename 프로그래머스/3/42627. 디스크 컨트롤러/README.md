29eb433b36ec656a18a5f2af004db14346930b6f
HeadedNowhere <nileyangdev@gmail.com>
2026-07-25 11:19:30 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 6.73 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,10 +1,10 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627?gad_source=1&gad_campaignid=23716289893&gbraid=0AAAAAC_c4nBAM_igVUb4HLAvC6ORuf-IM&gclid=Cj0KCQjwr4jSBhCSARIsAOX1E-LkrOk_7PhQxVWJ612c94MtxU5sRPwwwzL1klLXawkJenXeMJxNzgQaApYNEALw_wcB) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627?gad_source=1&gad_campaignid=23716289893&gbraid=0AAAAAC_c4nA2FtFGJR7mR10A2sFGRv_H6&gclid=CjwKCAjwmozTBhAeEiwAkEGZzhkVlHy0qOW9OPkFMd_hpz-G5rG6-hxv_A6Y-c86urGp2PoFKr1IIRoCTkQQAvD_BwE) 
 
 ### 성능 요약
 
-메모리: 76.8 MB, 시간: 4.26 ms
+메모리: 83.1 MB, 시간: 6.73 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 29일 17:18:51
+2026년 07월 25일 11:19:27
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,31 +1,62 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] jobs) {
-        int ans = 0;
-        //번호,요청시각,소요시간
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
-            if (o1[2] == o2[2]) {//소요시간
-                //요청시각
-                if (o1[1] == o2[1]) return o1[0] - o2[0];
-                else return o1[1] - o2[1];
-            }else return o1[2] - o2[2];
-        });
-        Arrays.sort(jobs, (o1, o2) -> o1[0] - o2[0]);
-        int doneCnt = 0;
-        int idx = 0;
-        int totalWait = 0;
-        int currentTime = 0;
-        while (doneCnt < jobs.length) {
-            while (idx < jobs.length && jobs[idx][0] <= currentTime) pq.offer(new int[] {idx, jobs[idx][0], jobs[idx++][1]});
-            if (pq.isEmpty()) {
-                currentTime = jobs[idx][0];
-            }else {
-                int[] polled = pq.poll();
-                currentTime += polled[2];
-                totalWait += (currentTime - polled[1]);
-                doneCnt++;
-            }
+        //정렬
+        int[][] jobsplus = new int[jobs.length][3];//인덱스추가
+        for (int i = 0; i < jobs.length; i++) {
+            int[] j = jobs[i];
+            jobsplus[i][0] = j[0];//요청
+            jobsplus[i][1] = j[1];//소요
+            jobsplus[i][2] = i;//인덱스..추가
         }
-        return totalWait / jobs.length;
+        
+        
+        //이제 본격작업..준비
+        Arrays.sort(jobsplus, (o1, o2) -> o1[0] - o2[0]);
+        int idx = 0;
+        int cnt = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
+            //0:요청시각 1:소요시간 2:작업번호
+            //소요시간리턴
+            if (o1[1] != o2[1]) return o1[1] - o2[1];
+            //아닌애들.. 동일한애들
+            if (o1[0] != o2[0]) return o1[0] - o2[0];
+            //이제 작업벊만
+            return o1[2] - o2[2];
+        });
+        int t = 0;
+        //다될떄까지
+        int sum = 0;
+        while (cnt < jobs.length) {
+            //시간맞는애들추가
+            while (idx < jobs.length && jobsplus[idx][0] <= t) {
+                pq.offer(new int[] {jobsplus[idx][0],jobsplus[idx][1],jobsplus[idx][2]});
+                idx++;
+            }
+            //처리
+            if (!pq.isEmpty()) {
+                int[] polled = pq.poll();
+                int requestedAt = polled[0];
+                int duration = polled[1];
+
+                int waited = t + duration - requestedAt;
+                sum += waited;
+                cnt++;
+                t += duration;
+            }
+            //시간보정
+            else if (idx < jobsplus.length && t < jobsplus[idx][0]) t = jobsplus[idx][0];
+        }
+        return sum / jobs.length;
     }
 }
+/**
+정렬을 사용 -> 큐생략?
+pq는 필요하긴할듯
+
+코드
+1.정렬
+2.while (!완성) 완성은 카운트
+    3.조건맞는애들 pq에넣기..while idx.. 시간조건이니까 나중에 시간비면처리해줘야함
+    넣은애중에 뽑아가지고 처리 시간계산 등
+*/
