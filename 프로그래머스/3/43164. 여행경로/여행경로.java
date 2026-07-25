418fb2f28de157e5313ca4a8c2e0f5296bbc7fd0
HeadedNowhere <nileyangdev@gmail.com>
2026-07-25 16:36:22 +0900

    [level 3] Title: 여행경로, Time: 1.08 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,39 +1,34 @@
 import java.util.*;
 class Solution {
-    Map<String, PriorityQueue<String>> ticket;
+    Map<String, PriorityQueue<String>> map;
     int targetLength;
-    List<String> ans = new ArrayList<>();
-    
+    List<String> ans;
     public String[] solution(String[][] tickets) {
-        this.ticket = new HashMap<>();
+        map = new HashMap<>();
         targetLength = tickets.length + 1;
         for (String[] t : tickets) {
-            String v1 = t[0];
-            String v2 = t[1];
-            ticket.computeIfAbsent(v1, k -> new PriorityQueue<>()).add(v2);
+            map.computeIfAbsent(t[0], k -> new PriorityQueue<>()).offer(t[1]);
         }
         List<String> path = new ArrayList<>();
         path.add("ICN");
         backtrack("ICN", path);
-        
         return ans.toArray(new String[0]);
     }
-    
     boolean backtrack(String current, List<String> path) {
-        if (path.size() == targetLength) {
+        if (path.size() ==targetLength) {
             ans = new ArrayList<>(path);
             return true;
         }
-        PriorityQueue<String> original = ticket.get(current);
-        if (original == null || original.isEmpty()) return false;
-        PriorityQueue<String> copy = new PriorityQueue<>(original);
+        PriorityQueue<String> origin = map.get(current);
+        if (origin == null) return false;
+        PriorityQueue<String> copy = new PriorityQueue<>(origin);
         while (!copy.isEmpty()) {
             String polled = copy.poll();
             path.add(polled);
-            original.remove(polled);
+            origin.remove(polled);
             if (backtrack(polled, path)) return true;
             path.remove(path.size() - 1);
-            original.offer(polled);
+            origin.offer(polled);
         }
         return false;
     }
