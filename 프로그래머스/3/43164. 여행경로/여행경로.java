47c0d367789d6b140b5822b9298e0104e3e36f18
HeadedNowhere <nileyangdev@gmail.com>
2026-07-25 14:21:45 +0900

    [level 3] Title: 여행경로, Time: 0.65 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,47 +1,40 @@
 import java.util.*;
 class Solution {
-    Map<String, PriorityQueue<String>> map;
-    List<String> ans;
-    int targetSize;
+    Map<String, PriorityQueue<String>> ticket;
+    int targetLength;
+    List<String> ans = new ArrayList<>();
+    
     public String[] solution(String[][] tickets) {
-        this.map = new HashMap<>();
-        this.ans = new ArrayList<>();
-        this.targetSize = tickets.length + 1;
+        this.ticket = new HashMap<>();
+        targetLength = tickets.length + 1;
         for (String[] t : tickets) {
             String v1 = t[0];
             String v2 = t[1];
-            map.computeIfAbsent(v1, k -> new PriorityQueue<>()).offer(v2);
+            ticket.computeIfAbsent(v1, k -> new PriorityQueue<>()).add(v2);
         }
         List<String> path = new ArrayList<>();
         path.add("ICN");
         backtrack("ICN", path);
+        
         return ans.toArray(new String[0]);
     }
+    
     boolean backtrack(String current, List<String> path) {
-        if (path.size() == targetSize) {
+        if (path.size() == targetLength) {
             ans = new ArrayList<>(path);
             return true;
         }
-        PriorityQueue<String> original = map.get(current);
+        PriorityQueue<String> original = ticket.get(current);
         if (original == null || original.isEmpty()) return false;
         PriorityQueue<String> copy = new PriorityQueue<>(original);
-        
         while (!copy.isEmpty()) {
-            //original에서지우고, path추가
-            String c = copy.poll();
-            original.remove(c);
-            path.add(c);
-            //백트래킹
-            if (backtrack(c, path)) return true;
-            //다시추가,path제거
-            original.offer(c);
+            String polled = copy.poll();
+            path.add(polled);
+            original.remove(polled);
+            if (backtrack(polled, path)) return true;
             path.remove(path.size() - 1);
+            original.offer(polled);
         }
         return false;
     }
 }
-/**
-1티켓을 모두써서
-2모두방문
-3사전순
-*/
