11c40d7f4626094f7142a3c0d2ccbe6bc3980427
HeadedNowhere <nileyangdev@gmail.com>
2026-07-25 10:38:33 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 33.74 ms, Memory: 150 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,46 +1,39 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        Map<String, String> parentsMap = new HashMap<>();
+        Map<String, Integer> wallet = new HashMap<>();
+        //부모관계지정
+        //referral 길이 enroll길이. referal[i]는 enrol[i]등록시킨사람
+        for (int i = 0; i < enroll.length; i++) {
+            parentsMap.put(enroll[i], referral[i]);
+        }
         
-        Map<String, Integer> moneyEarned = new HashMap<>();
-        Map<String, String> parent = new HashMap<>();
-        int len = enroll.length;
-        for (int i = 0; i < len; i++) parent.put(enroll[i], referral[i]);
-        // System.out.println(parent);
+        //seller길이=amount길이
         for (int i = 0; i < seller.length; i++) {
-            // System.out.printf("==== idx : %d====\n", i);
-            //계산
+            String dealer = seller[i];
             int total = amount[i] * 100;
-            String sold = seller[i];
-            while (total >= 1) {
-                if (sold.equals("-")) break;
-                int share = total * 1 / 10;
-                int mine = total - share;
-                if (share < 1) {
-                    moneyEarned.put(sold, moneyEarned.getOrDefault(sold, 0) + total);
-                    break;
-                }
-                moneyEarned.put(sold, moneyEarned.getOrDefault(sold, 0) + mine);
-                // System.out.printf("seller parent total share mine:%s %s %d %d %d\n", sold, parent.get(sold), total, share, mine);
-                total = share;
-                sold = parent.get(sold);
+            // System.out.printf("==== i : %d====\n", i);
+            //재귀
+            //처음판애도 똑같이 재귀가능.대칭가능..초기값만 필요하지 대칭깰필요는없다
+            //문제조건 절사한게 1원이면 자기가 다갖는대
+            while (total > 0) {
+                String parent = parentsMap.get(dealer);
+                int toParent = total * 1 / 10;//1원절삭가능
+                int mine = total - toParent;//부모가0원가져가면 다내거
+                //정산
+                // System.out.printf("dealer:%s, total:%d, parent:%s, toParent:%d, mine:%d\n", dealer, total, parent, toParent, mine);
+                wallet.put(dealer, wallet.getOrDefault(dealer, 0) + mine);
+                dealer = parent;
+                total = toParent;
             }
         }
         int[] ans = new int[enroll.length];
         for (int i = 0; i < enroll.length; i++) {
-            if (moneyEarned.get(enroll[i]) == null) continue;
-            ans[i] = moneyEarned.get(enroll[i]);
+            String name = enroll[i];
+            if (wallet.get(name) == null) ans[i] = 0;
+            else ans[i] = wallet.get(enroll[i]);
         }
         return ans;
-        
     }
 }
-/**
-노드 10000개
-간선 10000개
-
-금액계산해서 enroll순서대로 수익금나열
-
-절삭
-돈이 타고올라가는데 언제까지? 10%한게 1보다 작으면 거기까지
-*/
