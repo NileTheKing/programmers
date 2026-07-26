3a7754c1270ebb5d01d52914a785b564eb738ba2
HeadedNowhere <nileyangdev@gmail.com>
2026-07-26 11:07:54 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 2.54 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -1,100 +1,50 @@
 import java.util.*;
 class Solution {
-    int[][] directions = {{-1,0},{0,1},{1,0},{0,-1}};//0 2 수직 13수평
-    int m, n;
+    int[][] directions = {{-1,0},{1,0},{0,1},{0,-1}};
     public int solution(int[][] board) {
-        this.m = board.length;
-        this.n = board[0].length;
-        int[][][] visited = new int[m][n][2]; //[][][이놈은수직수평] 1수직0수평
-        Queue<Info> q = new LinkedList<>();
-        q.offer(new Info(0,0,-1, 0));
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                visited[i][j][0] = Integer.MAX_VALUE;
-                visited[i][j][1] = Integer.MAX_VALUE;
-            }
-        }
-        visited[0][0][0] = 0;
-        visited[0][0][1] = 0;
-        int min = Integer.MAX_VALUE;
-        while (!q.isEmpty()) {
-            Info polled = q.poll();
-            // System.out.printf("at (%d,%d), moved dir:%d, price:%d\n", polled.r, polled.c, polled.prevDirIdx, polled.price);
-            for (int i = 0; i < directions.length; i++) {
-                // System.out.printf("directions: %d\n", i);
-                int nr = polled.r + directions[i][0];
-                int nc = polled.c + directions[i][1];
-                //boundcheck
-                if (nr < 0 || nr >= m || nc < 0 || nc >= n) {
-                    // System.out.printf("bound\n");
-                    continue;
-                }
+        int m = board.length;
+        int n = board[0].length;
+        PriorityQueue<Info> pq = new PriorityQueue<>((o1,o2) -> o1.cost - o2.cost);
+        boolean[][][] visited = new boolean[m][n][2]; //0수평, 1수직
+        pq.offer(new Info(0,0,0,0));
+        pq.offer(new Info(0,0,0,1));
+        while (!pq.isEmpty()) {
+            Info polled = pq.poll();
+            if (visited[polled.r][polled.c][polled.prevDir]) continue;
+            visited[polled.r][polled.c][polled.prevDir] = true;
+            if (polled.r == m - 1 && polled.c == n - 1) return polled.cost;//종료검사는 여기서
+            // System.out.printf("(%d,%d) %d %d\n", polled.r, polled.c, polled.cost, polled.prevDir);
+            for (int[] d : directions) {
+                int nr = d[0] + polled.r;
+                int nc = d[1] + polled.c;
+                
+                // System.out.printf("%d %d\n", nr, nc);
+                //bound
+                if (nr < 0 || nr >= m || nc < 0 || nc >= n) continue;
+                //valid
                 if (board[nr][nc] == 1) continue;
-                if (i % 2 == 1) {//지금 수평으로 이동중
-                    // System.out.printf("moving horizontally\n");
-                    //visitedcheck
-                    //이전에 수직이었음(코너)
-                    if (polled.prevDirIdx != - 1 && polled.prevDirIdx % 2 == 0) {
-                        int nprice = polled.price + 600;
-                        if (visited[nr][nc][0] <= nprice) continue;
-                        visited[nr][nc][0] = nprice;
-                        if (nr == m - 1 && nc == n - 1) {
-                            // System.out.printf("found at price %d\n", nprice);
-                            min = Math.min(min, nprice);
-                        }
-                        q.offer(new Info(nr,nc, i, nprice));
-                    }
-                    //이전에 수직이었음(직선)
-                    else {
-                        int nprice =  polled.price + 100;
-                        if (visited[nr][nc][0] <= nprice) continue;
-                        visited[nr][nc][0] = nprice;
-                        if (nr == m - 1 && nc == n - 1) {
-                            // System.out.printf("found at price %d\n", nprice);
-                            min = Math.min(min, nprice);
-                        }
-                        q.offer(new Info(nr,nc, i, nprice));
-                    }
-                }else {
-                    //수직으로 이동중
-                    // System.out.printf("moving vertically\n");
-                    //이전에 수직이었음(직선)
-                    if (polled.prevDirIdx == - 1 || polled.prevDirIdx % 2 == 0) {
-                        int nprice=  polled.price + 100;
-                        if (visited[nr][nc][1] <= nprice) continue;
-                        visited[nr][nc][1] = nprice;
-                        if (nr == m - 1 && nc == n - 1) {
-                            // System.out.printf("found at price %d\n", nprice);
-                            min = Math.min(min, nprice);
-                        }
-                        q.offer(new Info(nr,nc, i, nprice));
-                    }
-                    //이전에 수평이었음
-                    else {
-                        int nprice=  polled.price + 600;
-                        if (visited[nr][nc][1] <= nprice) continue;
-                        visited[nr][nc][1] = nprice;
-                        if (nr == m - 1 && nc == n - 1) {
-                            // System.out.printf("found at price %d\n", nprice);
-                            min = Math.min(min, nprice);
-                        }
-                        q.offer(new Info(nr,nc, i, nprice));
-                    }
-                }
+                boolean goingHor = d[0] != 0 ? true : false;
+                boolean prevHor = polled.prevDir == 0 ? true : false;
+                //TT->straight 11
+                //TF -> corner 10
+                //FT -> corner 01
+                //FF -> straight 00
+                int deltaPrice = goingHor == prevHor ? 100 : 600;
+                pq.offer(new Info(nr,nc,polled.cost + deltaPrice, goingHor ? 0 : 1));
             }
         }
-        return min;
+        return -1;
     }
     class Info {
         int r;
         int c;
-        int prevDirIdx;
-        int price;
-        Info (int r, int c, int prevDirIdx, int price) {
+        int cost;
+        int prevDir;
+        Info  (int r, int c, int cost, int prevDir) {
             this.r = r;
             this.c = c;
-            this.prevDirIdx = prevDirIdx;
-            this.price = price;
+            this.cost = cost;
+            this.prevDir = prevDir;
         }
     }
 }
