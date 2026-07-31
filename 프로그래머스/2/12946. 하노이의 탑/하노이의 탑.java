37555f32bd8c61731376d0d2446cafc91c1edc99
HeadedNowhere <nileyangdev@gmail.com>
2026-07-31 13:39:11 +0900

    [level 2] Title: 하노이의 탑, Time: 1.25 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -1,27 +1,14 @@
 import java.util.*;
 class Solution {
-    List<int[]> ans;
+    List<int[]> ans = new ArrayList<>();
     public int[][] solution(int n) {
-        ans = new ArrayList<>();
-        hanoi(n, 1, 3, 2);
+        backtrack(n, 1, 3, 2);
         return ans.toArray(new int[0][]);
     }
-    void hanoi(int n, int from, int to, int via) {
+    void backtrack(int n, int from, int to, int via) {
         if (n == 0) return;
-        //n-1개를 via로
-        hanoi(n - 1, from, via, to); //n-1개를 이동..경유지로
-        ans.add(new int[] {from, to});//이제 1개남았으니.처리.
-        hanoi(n - 1, via, to, from);//결국 이거도 이동해야함
+        backtrack(n - 1, from, via, to);//일단 n- 1개 치움
+        ans.add(new int[] {from, to});
+        backtrack(n - 1, via, to, from);//n - 1개도 처리..다음으로
     }
 }
-/**
-ABC기둥 존재.
-가장큰 판 n -> C가야함
-    n-1개는 B로 이동해야함
-    n -> C로 이동
-    n-1는 C로 이동해야함
-
-처리완료 후 가장 큰 판 n-1 -> C가야함
-    n - 2개는 ~
-    반복..
-*/
