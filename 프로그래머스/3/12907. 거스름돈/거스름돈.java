e9a9584e9e96861afd1892d675503de3b1221546
HeadedNowhere <nileyangdev@gmail.com>
2026-08-03 17:04:42 +0900

    [level 3] Title: 거스름돈, Time: 8.69 ms, Memory: 57.7 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.java
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -1,12 +1,13 @@
 class Solution {
     public int solution(int n, int[] money) {
-        int[] dp = new int[n + 1];
-        dp[0] = 1;
-        int MOD = 1_000_000_007;
         
-        for (int m : money) {//세로축(행))
-            for (int i = m; i <= n; i++) { //가로축(열)
-                dp[i] = dp[i] + dp[i - m]; //두번째항이 열을보는것 첫번째항은 이전 열(이전 경우의수 누적)
+        int MOD = 1_000_000_007;
+        int[] dp = new int[n + 1];
+        dp[0] = 1;        
+        for (int m :  money) {
+            for (int i = m; i <= n; i++) {
+                // if (i - m <= 0) continue;
+                dp[i] += dp[i - m];
             }
         }
         return dp[n];
