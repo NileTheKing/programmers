899341aafc9310522fcaa657bb503f13a95de65b
HeadedNowhere <nileyangdev@gmail.com>
2026-08-04 10:55:05 +0900

    [level 3] Title: 양과 늑대, Time: 11.29 ms, Memory: 94.5 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,41 +1,38 @@
 import java.util.*;
 class Solution {
     int max = 0;
-    Map<Integer, List<Integer>> childMap;
     int[] info;
+    List<List<Integer>> tree;
     public int solution(int[] info, int[][] edges) {
-        this.childMap = new HashMap<>();
         this.info = info;
-        for (int i = 0; i < info.length; i++) childMap.put(i, new ArrayList<>());
+        tree = new ArrayList<>();
+        for (int i = 0; i < info.length; i++) tree.add(new ArrayList<>());
         for (int[] e : edges) {
-            childMap.get(e[0]).add(e[1]);
-            //트리..단방향
+            int v1 = e[0];
+            int v2 = e[1];
+            
+            tree.get(v1).add(v2);
         }
-
-        Set<Integer> toVisit = new HashSet<>();
-        toVisit.addAll(childMap.get(0));
-        backtrack(1,0, toVisit);
+        
+        backtrack(0, 1, 0, new HashSet<>());
         return max;
     }
-    void backtrack(int sheep, int wolf, Set<Integer> toVisit) {
-        max = Math.max(sheep, max);//매번갱신
-        //백트래킹..완탐
+    void backtrack(int current, int sheep, int wolf, Set<Integer> toVisit) {
+        //셀프추ㅏㄱ
+        max = Math.max(sheep, max);
+        toVisit.addAll(tree.get(current));
         
-        List<Integer> copy = new ArrayList<>(toVisit);
-        for (int next : copy) {//next는 노드번호
-            int nsheep = info[next] == 0 ? sheep + 1 : sheep;
-            int nwolf = wolf + info[next];
-            if (nwolf >= nsheep) continue;
-            //방문
-            toVisit.remove(next);
-            toVisit.addAll(childMap.get(next));
-            backtrack(nsheep, nwolf, toVisit);
-            toVisit.add(next);
-            toVisit.removeAll(childMap.get(next));
+        //순회
+        for (int nei : new HashSet<>(toVisit)) {
+            int nwolf = wolf + info[nei];
+            int nsheep = sheep + (info[nei] == 0 ? 1 : 0);
+            
+            if (nsheep <= nwolf) continue;
+            
+            toVisit.remove(nei);
+            backtrack(nei, nsheep, nwolf, toVisit);
+            toVisit.add(nei);
         }
-        
+        toVisit.removeAll(tree.get(current));
     }
 }
-/**
-완탐..매번갱신. 종료조건은 없어서 종료되거나 뭐 명시적으로 활용하거나...
-*/
