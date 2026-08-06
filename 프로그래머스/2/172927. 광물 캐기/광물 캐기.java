3a7530c5a72598107d66483638a226850fef40ff
HeadedNowhere <nileyangdev@gmail.com>
2026-08-06 12:02:48 +0900

    [level 2] Title: 광물 캐기, Time: 68.26 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,96 +1,101 @@
 import java.util.*;
 class Solution {
-    int min = Integer.MAX_VALUE;
     Map<String, Integer> map;
+    String[] minerals;
+    int min = Integer.MAX_VALUE;
     public int solution(int[] picks, String[] minerals) {
-        initializeMap();
-        int[] choices = new int[minerals.length];
-        Arrays.fill(choices, -1);
-        backtrack(picks, minerals, 0, choices);
+        init();
+        this.minerals = minerals;
+        int targetSize = (minerals.length + 5 - 1) / 5;
+        int[] choice = new int[targetSize];
+        Arrays.fill(choice, -1);
+        backtrack(0, choice, picks);
+        
         return min;
     }
-    public void backtrack(int[] picks, String[] minerals, int choiceIdx, int[] choices) {
-        // System.out.printf("idx %d\n", choiceIdx);
-        if (usedAll(picks) || choiceIdx == (minerals.length + 5 - 1)/ 5) { //10개 -> choiceIdx == 2종료. 0indexed
-
-            calculate(minerals, choices);
+    
+    void backtrack(int idx, int[] choice, int[] picks) {
+        if (idx >= choice.length) {
+            // System.out.printf("=====calculating END======\n");
+            calculate(choice);
             return;
         }
-        // if (usedAll(picks)) {System.out.printf("returning cuz of empty\n");return;}
-        // System.out.printf("idx %d\n", choiceIdx);
-        //0 1 2중에 고름..갯수가 있어야 고름 돌 철 다야
+        if (allEmpty(picks)) {
+            // System.out.printf("======calculating NO PICKS=======\n");
+            calculate(choice);
+            return;
+        }
+        // System.out.printf("======\n");
+        // System.out.printf("idx = %d\n", idx);
+        
         if (picks[0] > 0) {
-            choices[choiceIdx] = 0;
             picks[0]--;
-            backtrack(picks, minerals, choiceIdx + 1, choices);
+            choice[idx] = 0;
+            backtrack(idx + 1, choice, picks);
             picks[0]++;
         }
         if (picks[1] > 0) {
-            choices[choiceIdx] = 1;
             picks[1]--;
-            backtrack(picks, minerals, choiceIdx + 1, choices);
+            choice[idx] = 1;
+            backtrack(idx + 1, choice, picks);
             picks[1]++;
         }
         if (picks[2] > 0) {
-            choices[choiceIdx] = 2;
             picks[2]--;
-            backtrack(picks, minerals, choiceIdx + 1, choices);
+            choice[idx] = 2;
+            backtrack(idx + 1, choice, picks);
             picks[2]++;
         }
-        //돌려놓을 필요? picks는 같은레벨의 다른애들한테 영향주는데.. choices0 1은 ㅇ덮어씅지느끼 상관없고.. 2는 어차피 이 뒤에 영향받을애가 다음 뎁스(레벨)인데 걘 영향받는게당연하고 이젠 뎁스로 돌아가는 리턴에서는..상관없지 리턴만 계속될텐데...
-        return;
     }
-    public void calculate(String[] minerals, int[] choices) {
-        // System.out.printf("===calculating===\n");
-        // for (int c : choices) System.out.println(c);
-        int pirodo = 0;
-        for (int i = 0; i < choices.length; i++) {
-            //고른 곡괭이 choices[i]로 minerals 5개 캐서 계산후 max계산.
-            int pick = choices[i];
-            int startJ = 5 * i;
-            if (pick == -1) break;
-            for (int j = 0; j < 5; j++) {
-                if (startJ + j >= minerals.length) break;
-                String mineral = minerals[startJ + j];
-                String key = pick + mineral;
-                // System.out.printf("spent %d\n", map.get(key));
-                pirodo += map.get(key);
+    void calculate(int[] choice) {
+        //choice대로 minerals5개씩
+        int res = 0;
+        Outter : for (int i = 0; i < choice.length; i++) {
+            // System.out.printf("--------\n");
+            int turn = 0;
+            if (choice[i] == -1) break;
+            for(int j = 0; j < 5; j++) {
+                int idx = 5 * i + j;
+                if (idx >= minerals.length) break;
+                int key = getKey(minerals[idx]);
+                int pirodo = map.get(String.valueOf(choice[i])+"+"+String.valueOf(key));
+                // System.out.printf("idx:%d, key:%d, realkey:%s\n",idx,  key, String.valueOf(choice[i])+"+"+String.valueOf(key));
+                res+=pirodo;
+                // System.out.printf("turn: %d\n", turn);
             }
+            // res += turn;
+            // System.out.printf("i=%d,choice:%d, res= %d\n", i,choice[i], res);
         }
-        // System.out.printf("total = %d\n", pirodo);
-        min = Math.min(min, pirodo);
+        // System.out.printf("res:%d\n--------\n", res);
+        min = Math.min(min, res);
         return;
-        
     }
-    public void initializeMap() {
+    void init() {
         map = new HashMap<>();
-        map.put("2stone", 1);
-        map.put("2iron", 5);
-        map.put("2diamond", 25);
+        map.put("0+0", 1);
+        map.put("0+1", 1);
+        map.put("0+2", 1);
         
-        map.put("1stone", 1);
-        map.put("1iron", 1);
-        map.put("1diamond", 5);
+        map.put("1+0", 5);
+        map.put("1+1", 1);
+        map.put("1+2", 1);
         
-        map.put("0stone", 1);
-        map.put("0iron", 1);
-        map.put("0diamond", 1);
+        map.put("2+0", 25);
+        map.put("2+1", 5);
+        map.put("2+2", 1);
         
         return;
     }
-    public boolean usedAll(int[] picks) {
-        for (int p : picks) if (p != 0) return false;
+    int getKey(String str) {
+        if (str.equals("diamond")) return 0;
+        if (str.equals("iron")) return 1;
+        if (str.equals("stone")) return 2;
+        return -1;
+    }
+    boolean allEmpty(int[] picks) {
+        for (int p : picks) {
+            if (p > 0) return false;
+        }
         return true;
     }
 }
-/**
-곡괭이가있다. 종류3개
-갯수는 각각 0-5사이.캘떄 피로도소모.. 표존재
-꺼낸 곡괭이는 없어질떄까지 -5개연속 캐야함
-
-5개단위로 선택해야한다. 곡괭이 다쓸 떄 까지 다캘 떄 까지.
-다캘떄 최소피로도 return
-
-백트래킹
-총 10번의 선택
-*/
